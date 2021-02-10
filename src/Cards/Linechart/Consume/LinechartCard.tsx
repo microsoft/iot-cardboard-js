b29e37ced1b80dd8f59d9b04840726639f60cc7f
Nick Usoff <niusoff@microsoft.com>
2021-02-10 14:56:36 -0800

    removed unused imports from react

--- src/Cards/Linechart/Consume/LinechartCard.tsx
+++ src/Cards/Linechart/Consume/LinechartCard.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ClientLinechart from 'tsiclient/LineChart';
 import './LinechartCard.scss';
 import 'tsiclient/tsiclient.css';
