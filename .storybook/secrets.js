0a5af7e8d75e799513e0d565cf8e91386e9a15fc
Cory Crowley <ccrowley96@gmail.com>
2021-03-02 10:28:13 -0800

    cleaned secrets

--- .storybook/secrets.js
+++ .storybook/secrets.js
@@ -9,13 +9,12 @@ export const AuthenticationParameters = {
         }
     },
     tsi: {
-        environmentFqdn:
-            '10000000-0000-0000-0000-100000000109.env.timeseries.azure.com',
+        environmentFqdn: 'ENVFQDN_PLACEHOLDER',
         aadParameters: {
-            authority: 'https://login.microsoftonline.com/organizations',
-            clientId: 'e7e88070-28a1-43a3-9704-d8b986eb5f60',
-            scope: 'https://api.timeseries.azure.com//.default',
-            redirectUri: 'https://adtexplorer-tsi-local.azurewebsites.net'
+            authority: 'AUTHORITY_PLACEHOLDER',
+            clientId: 'CLIENTID_PLACEHOLDER',
+            scope: 'SCOPE_PLACEHOLDER',
+            redirectUri: 'REDIRECTURI_PLACEHOLDER'
         }
     }
 };
