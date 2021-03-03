879da2d5317990eff863696b3e6a15fc8a898812
Matt Darsney <matdar@microsoft.com>
2021-03-03 11:47:04 -0800

    Try to load user secrets, fall back to placeholder (#20)
    * secrets.js tries to load secrets.user and falls back to secrets.placeholder, plus methods for consumption, and adding secrets.user to gitignore
    
    * refactor auth parameter logic into a hook
    
    * spread properties from user onto placeholder secrets

--- src/Cards/Linechart/Consume/LinechartCard.stories.tsx
+++ src/Cards/Linechart/Consume/LinechartCard.stories.tsx
@@ -3,9 +3,9 @@ import Linechart from './LinechartCard';
 import MockAdapter from '../../../Adapters/MockAdapter';
 import TsiAdapter from '../../../Adapters/TsiAdapter';
 import { SearchSpan } from '../../../Models/Classes/SearchSpan';
-import { AuthenticationParameters } from '../../../../.storybook/secrets';
 import MsalAuthService from '../../../Models/Services/MsalAuthService';
 import { Theme } from '../../../Models/Constants/Enums';
+import useAuthParams from '../../../../.storybook/useAuthParams';
 
 export default {
     title: 'Linechart/Consume'
@@ -49,6 +49,7 @@ export const NoData = (args, { globals: { theme } }) => (
 );
 
 export const TsiData = (args, { globals: { theme } }) => {
+    const authenticationParameters = useAuthParams();
     const tsiId = 'df4412c4-dba2-4a52-87af-780e78ff156b';
     const tsiProperties = ['value'];
     const tsiSearchSpan = new SearchSpan(
@@ -56,7 +57,9 @@ export const TsiData = (args, { globals: { theme } }) => {
         new Date('2017-05-20T20:00:00Z'),
         '6h'
     );
-    return (
+    return !authenticationParameters ? (
+        <div></div>
+    ) : (
         <div style={chartCardStyle}>
             <Linechart
                 theme={theme}
@@ -65,9 +68,9 @@ export const TsiData = (args, { globals: { theme } }) => {
                 properties={tsiProperties}
                 adapter={
                     new TsiAdapter(
-                        AuthenticationParameters.tsi.environmentFqdn,
+                        authenticationParameters.tsi.environmentFqdn,
                         new MsalAuthService(
-                            AuthenticationParameters.tsi.aadParameters
+                            authenticationParameters.tsi.aadParameters
                         )
                     )
                 }
