7bc668d88df47e43e822c4f67d387681e9f99aec
Cory Crowley <ccrowley96@gmail.com>
2021-03-09 15:36:20 -0800

    Added new hooks to hooks index

--- src/Models/Hooks/index.ts
+++ src/Models/Hooks/index.ts
@@ -1,1 +1,4 @@
 export { default as useGuid } from './useGuid';
+export { default as useAdapter } from './useAdapter';
+export { default as useCancellablePromise } from './useCancellablePromise';
+export { default as useLongPoll } from './useLongPoll';
