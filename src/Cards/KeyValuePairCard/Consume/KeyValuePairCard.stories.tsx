7cacb842624807a895fea1a33b95a057911626bc
Cory Crowley <ccrowley96@gmail.com>
2021-06-07 16:09:12 -0700

    Added back UsingCustomKVPAdapter to KeyValuePairCard.stories.tsx

--- src/Cards/KeyValuePairCard/Consume/KeyValuePairCard.stories.tsx
+++ src/Cards/KeyValuePairCard/Consume/KeyValuePairCard.stories.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import MockAdapter from '../../../Adapters/MockAdapter';
+import KeyValuePairAdapterData from '../../../Models/Classes/AdapterDataClasses/KeyValuePairAdapterData';
+import AdapterMethodSandbox from '../../../Models/Classes/AdapterMethodSandbox';
+import { IKeyValuePairAdapter } from '../../../Models/Constants/Interfaces';
+import { KeyValuePairData } from '../../../Models/Constants/Types';
 import KeyValuePairCard from './KeyValuePairCard';
+import { useStableGuidRng } from '../../../Models/Context/StableGuidRngProvider';
 
 export default {
     title: 'KeyValuePairCard/Consume',
@@ -39,3 +44,48 @@ export const MockOverflow = (_args, { globals: { theme } }) => (
         />
     </div>
 );
+
+export const UsingCustomKVPAdapter = (
+    _args,
+    { globals: { theme, locale }, parameters: { defaultCardWrapperStyle } }
+) => {
+    const seededRng = useStableGuidRng();
+
+    // Create adapter object adhering to IKeyValuePairAdapter interface
+    const customAdapterUsingInterface: IKeyValuePairAdapter = {
+        getKeyValuePairs: async (_id, properties, _additionalParameters?) => {
+            // Construct AdapterMethodSandbox class to wrap custom logic in error handling sandbox
+            const adapterMethodSandbox = new AdapterMethodSandbox();
+
+            // Use the safelyFetchData method to make your adapter call.
+            // if the adapter logic fails, this method will register the error and bubble
+            // the error up to be shown in the card, rather than failing entirely
+            return await adapterMethodSandbox.safelyFetchData(async () => {
+                const kvps = properties.map((prop, idx) => {
+                    const kvp: KeyValuePairData = {
+                        key: prop,
+                        value: seededRng(),
+                        timestamp: new Date(
+                            new Date('01/01/2021').getTime() + idx * 1000
+                        )
+                    };
+                    return kvp;
+                });
+
+                return new KeyValuePairAdapterData(kvps);
+            });
+        }
+    };
+
+    return (
+        <div style={defaultCardWrapperStyle}>
+            <KeyValuePairCard
+                id="kvp-tester"
+                theme={theme}
+                properties={['Custom KeyValuePair Adapter example']}
+                adapter={customAdapterUsingInterface}
+                locale={locale}
+            />
+        </div>
+    );
+};
