32f227e2fd6c30dbd1d51315949cc630d84174be
Irem Gokce Aydin <iremay@microsoft.com>
2021-08-23 08:41:16 -0700

    typing fix and cleaned comments

--- src/Cards/ADTModelUploaderCard/ADTModelUploaderCard.types.ts
+++ src/Cards/ADTModelUploaderCard/ADTModelUploaderCard.types.ts
@@ -1,8 +1,10 @@
+import ADTAdapter from '../../Adapters/ADTAdapter';
 import { ADTModelsData } from '../../Models/Classes/AdapterDataClasses/ADTUploadData';
 import AdapterResult from '../../Models/Classes/AdapterResult';
 import { IStandaloneConsumeCardProps } from '../../Models/Constants/Interfaces';
 
 export interface ADTModelUploaderCardProps extends IStandaloneConsumeCardProps {
+    adapter: ADTAdapter;
     hasUploadButton: boolean;
     hasMessageBar: boolean;
     onUploadFinish?: (adapterResult: AdapterResult<ADTModelsData>) => void;
