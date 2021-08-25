8d287dc95fd0b706d82f93df86c3b11ff7943deb
Irem Gokce Aydin <iremay@microsoft.com>
2021-08-25 14:34:24 -0700

    new model form added and some styling changes

--- src/Cards/CompositeCards/ADTModelAuthoring/ADTModelAuthoringCard.tsx
+++ src/Cards/CompositeCards/ADTModelAuthoring/ADTModelAuthoringCard.tsx
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import BaseCompositeCard from '../BaseCompositeCard/Consume/BaseCompositeCard';
 import { useTranslation } from 'react-i18next';
 import './ADTModelAuthoringCard.scss';
 import { ADTModelAuthoringCardProps } from './ADTModelAuthoringCard.types';
 import StepperWizard from '../../../Components/StepperWizard/StepperWizard';
-import { IStepperWizardStep } from '../../../Models/Constants/Interfaces';
+import {
+    DTModel,
+    IADTModel,
+    IStepperWizardStep
+} from '../../../Models/Constants/Interfaces';
 import ADTModelUploaderCard from '../../ADTModelUploaderCard/ADTModelUploaderCard';
 import { DefaultButton, Icon, PrimaryButton } from '@fluentui/react';
 import {
+    FormMode,
     ModelAuthoringModes,
     ModelAuthoringSteps
 } from '../../../Models/Constants/Enums';
+import ModelCreate from '../../../Components/ModelCreate/ModelCreate';
+import { DTDLModel } from '../../../Models/Classes/DTDL';
+import AdapterResult from '../../../Models/Classes/AdapterResult';
+import { ADTAdapterModelsData } from '../../../Models/Classes/AdapterDataClasses/ADTAdapterData';
 
 const ADTModelAuthoringCard: React.FC<ADTModelAuthoringCardProps> = ({
     adapter,
@@ -18,7 +27,8 @@ const ADTModelAuthoringCard: React.FC<ADTModelAuthoringCardProps> = ({
     locale,
     localeStrings,
     title,
-    onCancel
+    onCancel,
+    existingModelIds
 }) => {
     const { t } = useTranslation();
     const [
@@ -28,6 +38,8 @@ const ADTModelAuthoringCard: React.FC<ADTModelAuthoringCardProps> = ({
     const [authoringSteps, setAuthoringSteps] = useState<ModelAuthoringSteps>(
         ModelAuthoringSteps.SelectType
     );
+    const existingModelIdsRef = useRef(existingModelIds ?? []);
+    const newModelComponentRef = useRef();
 
     const steps: Array<IStepperWizardStep> = [
         {
@@ -43,6 +55,21 @@ const ADTModelAuthoringCard: React.FC<ADTModelAuthoringCardProps> = ({
             label: t('modelAuthoring.publish')
         }
     ];
+
+    const handleNewModelClick = async (model: DTDLModel) => {
+        const resolvedModels: AdapterResult<ADTAdapterModelsData> = await adapter.createADTModels(
+            [model.trimmedCopy() as DTModel]
+        );
+        // if (resolvedModels.getCatastrophicError()?.rawError) {
+        //     setErrorMessage(
+        //         (resolvedModels.getCatastrophicError().rawError as any).response
+        //             ?.data?.error?.message
+        //     );
+        // } else {
+        const resolvedModel = resolvedModels.getData()?.[0] as IADTModel;
+        console.log(resolvedModel);
+        // }
+    };
 
     return (
         <div className="cb-model-authoring-card-wrapper">
@@ -109,7 +136,7 @@ const ADTModelAuthoringCard: React.FC<ADTModelAuthoringCardProps> = ({
                                     description="Using built-in modules to create a DTDL model, no code is needed."
                                     onClick={() => {
                                         setAuthoringMode(
-                                            ModelAuthoringModes.UploadFiles
+                                            ModelAuthoringModes.BuildForm
                                         );
                                         setAuthoringSteps(
                                             ModelAuthoringSteps.Review
@@ -118,24 +145,47 @@ const ADTModelAuthoringCard: React.FC<ADTModelAuthoringCardProps> = ({
                                 />
                             </div>
                         </div>
+                    ) : authoringMode === ModelAuthoringModes.UploadFiles ? (
+                        <div className="cb-model-authoring-card-upload-files">
+                            <div
+                                style={{
+                                    fontSize: 18,
+                                    fontWeight: 600,
+                                    padding: '20px 0px'
+                                }}
+                            >
+                                Upload DTDL files
+                            </div>
+                            <ADTModelUploaderCard
+                                adapter={adapter}
+                                theme={theme}
+                                locale={locale}
+                                hasUploadButton={false}
+                                hasMessageBar={true}
+                            />
+                        </div>
                     ) : (
-                        authoringMode === ModelAuthoringModes.UploadFiles && (
-                            <div className="cb-model-authoring-card-upload-files">
+                        authoringMode === ModelAuthoringModes.BuildForm && (
+                            <div className="cb-model-authoring-card-new-model">
                                 <div
                                     style={{
-                                        fontSize: 18,
-                                        fontWeight: 600,
-                                        padding: '20px 0px'
+                                        padding: '10px 0px'
                                     }}
-                                >
-                                    Upload DTDL files
-                                </div>
-                                <ADTModelUploaderCard
-                                    adapter={adapter}
-                                    theme={theme}
+                                ></div>
+                                <ModelCreate
                                     locale={locale}
-                                    hasUploadButton={false}
-                                    hasMessageBar={true}
+                                    modelToEdit={null}
+                                    existingModelIds={
+                                        existingModelIdsRef.current
+                                    }
+                                    onCancel={() =>
+                                        setAuthoringSteps(
+                                            ModelAuthoringSteps.SelectType
+                                        )
+                                    }
+                                    onPrimaryAction={handleNewModelClick}
+                                    formControlMode={FormMode.New}
+                                    ref={newModelComponentRef}
                                 />
                             </div>
                         )
--- src/Cards/CompositeCards/ADTModelAuthoring/ADTModelAuthoringCard.types.ts
+++ src/Cards/CompositeCards/ADTModelAuthoring/ADTModelAuthoringCard.types.ts
@@ -4,4 +4,5 @@ import { IConsumeCompositeCardProps } from '../../../Models/Constants/Interfaces
 export interface ADTModelAuthoringCardProps extends IConsumeCompositeCardProps {
     adapter: ADTAdapter;
     onCancel: () => void;
+    existingModelIds?: Array<string>;
 }
