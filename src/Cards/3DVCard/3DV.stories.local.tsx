b1cd6358d16cb82fb4469efa3ece99943bbac640
Sam Gates <samgates@microsoft.com>
2021-08-27 16:09:40 +0100

    Added 3DV card with placeholder model

---
+++ src/Cards/3DVCard/3DV.stories.local.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ThreeDVCard from './3DVCard';
+
+export default {
+    title: '3DV/3DVCard'
+};
+
+export const Cube = () => (
+    <div>
+        <ThreeDVCard/>
+    </div>
+);
---
+++ src/Cards/3DVCard/SceneComponent.tsx
@@ -0,0 +1,44 @@
+import { Engine, Scene } from "babylonjs";
+import React, { useEffect, useRef } from "react";
+
+export default (props: any) => {
+  const reactCanvas = useRef(null);
+  const { antialias, engineOptions, adaptToDeviceRatio, sceneOptions, onRender, onSceneReady, ...rest } = props;
+
+  useEffect(() => {
+    if (reactCanvas.current) {
+      const engine = new Engine(reactCanvas.current, antialias, engineOptions, adaptToDeviceRatio);
+      const scene = new Scene(engine, sceneOptions);
+      if (scene.isReady()) {
+        props.onSceneReady(scene);
+      } else {
+        scene.onReadyObservable.addOnce((scene) => props.onSceneReady(scene));
+      }
+
+      engine.runRenderLoop(() => {
+        if (typeof onRender === "function") {
+          onRender(scene);
+        }
+        scene.render();
+      });
+
+      const resize = () => {
+        scene.getEngine().resize();
+      };
+
+      if (window) {
+        window.addEventListener("resize", resize);
+      }
+
+      return () => {
+        scene.getEngine().dispose();
+
+        if (window) {
+          window.removeEventListener("resize", resize);
+        }
+      };
+    }
+  }, [reactCanvas]);
+
+  return <canvas ref={reactCanvas} {...rest} style={{width: '100%', height: '100%'}} />;
+};
