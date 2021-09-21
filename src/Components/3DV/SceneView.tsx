f3bdb2924fc0b759d01fb174669cb88653e1c9dd
Paul Tallett <paul.tallett@microsoft.com>
2021-09-21 09:15:50 +0100

    Tidy up

--- src/Components/3DV/SceneView.tsx
+++ src/Components/3DV/SceneView.tsx
@@ -14,10 +14,25 @@ async function loadPromise(root, file, scene, onProgress: any, onError: any): Pr
     });
 }
 
-export const SceneView: React.FC<SceneViewProps> = ({modelUrl, cameraRadius, cameraCenter, labels}) => {
+export const SceneView: React.FC<SceneViewProps> = ({ modelUrl, cameraRadius, cameraCenter, labels }) => {
     const [isLoading, setIsLoading] = useState(true);
     const [loadProgress, setLoadProgress] = useState(0);
     const [scene, setScene] = useState(null);
+
+    function onProgress(e: any) {
+        if (e.total) {
+            setLoadProgress(e.loaded / e.total);
+        } else {
+            setLoadProgress(0);
+        }
+    }
+    
+    async function load(root: string, file: string, scene: any) {
+        const assets = await loadPromise(root, file, scene, (e) => onProgress(e), () => setIsLoading(undefined));
+        assets.addAllToScene();
+        setScene(scene);
+        setIsLoading(false);
+    }
 
     useEffect(() => {
         const canvas = document.getElementById('canvas') as HTMLCanvasElement;
@@ -33,71 +48,55 @@ export const SceneView: React.FC<SceneViewProps> = ({modelUrl, cameraRadius, cam
         new BABYLON.HemisphericLight('light', new BABYLON.Vector3(1, -1, 0), tempScene);
         new BABYLON.HemisphericLight('light', new BABYLON.Vector3(-1, 1, 0), tempScene);
 
-        async function load (root: string, file: string, scene: any) {
-            const assets = await loadPromise(root, file, scene, (e) => onProgress(e), () => setIsLoading(undefined));
-            assets.addAllToScene();
-            setScene(scene);
-            setIsLoading(false);
-        }
-
         if (modelUrl) {
             const n = modelUrl.lastIndexOf('/') + 1;
-            const root = modelUrl.substring(0, n);
-            const file = modelUrl.substring(n);
-            load(root, file, tempScene);
+            load(modelUrl.substring(0, n), modelUrl.substring(n), tempScene);
         }
         
         engine.runRenderLoop(() => { tempScene.render(); });
-        window.addEventListener('resize', () => { engine.resize(); });
 
-        function onProgress(e: any) {
-            if (e.total) {
-                setLoadProgress(e.loaded/e.total);
-            } else {
-                setLoadProgress(0);
+        const resize = () => { engine.resize(); };
+        window.addEventListener('resize', resize);
+        return () => window.removeEventListener('resize', resize);
+    }, [modelUrl]);
+
+    useEffect(() => {
+        const rects: any[] = [];
+        const advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+        if (labels && scene) {
+            labels.forEach(item => {
+                const rect = new GUI.Rectangle();
+                rect.width = '150px';
+                rect.height = '100px';
+                rect.cornerRadius = 10;
+                rect.color = '#364944';
+                rect.thickness = 1;
+                rect.background = '#161B36';
+                rects.push(rect);
+                advancedTexture.addControl(rect);
+                
+                const label = new GUI.TextBlock();
+                label.color = 'white'
+                label.text = item.metric + '\n\n' + item.value.toFixed(5);
+                rect.addControl(label);
+                const targetMesh = scene?.meshes?.find(mesh => mesh.id === item.meshId);
+                if (targetMesh) {
+                    rect.linkWithMesh(targetMesh);
+                }
+            });
+        }
+
+        return () => {
+            for (const rect of rects) {
+                advancedTexture.removeControl(rect);
             }
         }
-      }, [modelUrl]);
 
-      useEffect(() => {
-          const rects: any[] = [];
-          const advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
-            if(labels && scene) {
-                labels.forEach(item => {
-                    const rect = new GUI.Rectangle();
-                    rect.width = '150px';
-                    rect.height = '100px';
-                    rect.cornerRadius = 10;
-                    rect.color = '#364944';
-                    rect.thickness = 1;
-                    rect.background = '#161B36';
-                    rects.push(rect);
-                    advancedTexture.addControl(rect);
-                
-                    const label = new GUI.TextBlock();
-                    label.color = 'white'
-                    label.text = item.metric + '\n\n' + item.value.toFixed(5);
-                    rect.addControl(label);
-                    const targetMesh = scene?.meshes.find(mesh => mesh.id === item.meshId);
-                    if (targetMesh) {
-                        rect.linkWithMesh(targetMesh); 
-                    }
-                });
-            }
-
-            return () => {
-                for (const rect of rects) {
-                    advancedTexture.removeControl(rect); 
-                }
-            }
-
-      }, [labels, scene]);
-
-
+    }, [labels, scene]);
 
     return (
         <div className='sceneview-container'>
-            <canvas id='canvas' touch-action='none'/>
+            <canvas id='canvas' touch-action='none' />
             {isLoading && <ProgressIndicator className='sceneview-progressbar' styles={{ itemDescription: { color: 'white', fontSize: 26, marginTop: 10 } }}
                 description={`Loading (${Math.floor(loadProgress * 100)}%)...`} percentComplete={loadProgress} barHeight={10} />}
             {isLoading === undefined && <div className='sceneview-errormessage' style={{}}>Error loading model</div>}
