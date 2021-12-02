c4653df76aea6863105db89e3299e9784b7853ab
darsney <matdar@microsoft.com>
2021-12-02 14:06:52 -0800

    couple more stories

--- src/Cards/GeoSpatial/Consume/GeoSpatial.stories.tsx
+++ src/Cards/GeoSpatial/Consume/GeoSpatial.stories.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
+import useAuthParams from '../../../../.storybook/useAuthParams';
+import ADTAdapter from '../../../Adapters/ADTAdapter';
+import MsalAuthService from '../../../Models/Services/MsalAuthService';
 import MockAdapter from '../../../Adapters/MockAdapter';
+import { CardErrorType } from '../../../Models/Constants';
 import GeoSpatial from './GeoSpatial';
 
 export default {
@@ -20,11 +24,18 @@ const basicGeoData = [
     }
 ];
 
+const adtGeoData = [
+    {
+        id: 'PasteurizationMachine_A02',
+        properties: ['InFlow', 'OutFlow']
+    }
+];
+
 export const Mock = (
     args,
     { globals: { theme, locale }, parameters: { wideCardWrapperStyle } }
 ) => {
-    const [properties, setProperties] = useState({
+    const [properties] = useState({
         properties: basicGeoData[0].properties,
         id: basicGeoData[0].id
     });
@@ -39,6 +50,59 @@ export const Mock = (
                 theme={theme}
                 locale={locale}
                 adapter={new MockAdapter()}
+            />
+        </div>
+    );
+};
+
+export const FailedToGetData = (
+    args,
+    { globals: { theme, locale }, parameters: { wideCardWrapperStyle } }
+) => {
+    return (
+        <div style={wideCardWrapperStyle}>
+            <GeoSpatial
+                id={adtGeoData[0].id}
+                pollingIntervalMillis={1000}
+                properties={adtGeoData[0].properties}
+                title={'Current Boat Location'}
+                theme={theme}
+                locale={locale}
+                adapter={
+                    new MockAdapter({
+                        mockError: CardErrorType.DataFetchFailed
+                    })
+                }
+            />
+        </div>
+    );
+};
+
+export const ADT = (
+    args,
+    { globals: { theme, locale }, parameters: { wideCardWrapperStyle } }
+) => {
+    const authenticationParameters = useAuthParams();
+
+    return !authenticationParameters ? (
+        <div></div>
+    ) : (
+        <div style={wideCardWrapperStyle}>
+            <GeoSpatial
+                id={adtGeoData[0].id}
+                pollingIntervalMillis={1000}
+                properties={adtGeoData[0].properties}
+                title={'Current Boat Location'}
+                theme={theme}
+                locale={locale}
+                adapter={
+                    new ADTAdapter(
+                        authenticationParameters.adt.hostUrl,
+                        new MsalAuthService(
+                            authenticationParameters.adt.aadParameters
+                        )
+                    )
+                }
             />
         </div>
     );
