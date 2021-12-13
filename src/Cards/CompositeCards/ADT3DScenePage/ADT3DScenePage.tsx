b9fec05893feaa61ab97d66a2d33229b2b7f6cf5
samjosephgates <54577812+samjosephgates@users.noreply.github.com>
2021-12-13 11:25:32 +0000

    Samgates/buildercardfix (#147)
    * Fixed callback on builder card when mesh clicked
    
    * lint fixes

--- src/Cards/CompositeCards/ADT3DScenePage/ADT3DScenePage.tsx
+++ src/Cards/CompositeCards/ADT3DScenePage/ADT3DScenePage.tsx
@@ -160,6 +160,10 @@ const ADT3DSceneBuilderCompositeComponent: React.FC<IADT3DSceneBuilderProps> = (
 }) => {
     const { t } = useTranslation();
 
+    const onMeshSelected = (selectedMeshes) => {
+        console.log(selectedMeshes);
+    };
+
     return (
         <BaseCompositeCard
             title={title}
@@ -200,6 +204,9 @@ const ADT3DSceneBuilderCompositeComponent: React.FC<IADT3DSceneBuilderProps> = (
                             <ADT3DBuilderCard
                                 title="3D Builder"
                                 modelUrl={scene.assets[0].url}
+                                onMeshSelected={(selectedMeshes) =>
+                                    onMeshSelected(selectedMeshes)
+                                }
                             />
                         </div>
                     </BaseCompositeCard>
