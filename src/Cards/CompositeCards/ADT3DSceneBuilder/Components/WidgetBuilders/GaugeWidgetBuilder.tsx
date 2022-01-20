9a949228e7043914d5e16346914c938d3d21b90f
Cory Crowley <ccrowley96@gmail.com>
2022-01-19 21:18:55 -0800

    Added single warning break point to gauge form

--- src/Cards/CompositeCards/ADT3DSceneBuilder/Components/WidgetBuilders/GaugeWidgetBuilder.tsx
+++ src/Cards/CompositeCards/ADT3DSceneBuilder/Components/WidgetBuilders/GaugeWidgetBuilder.tsx
@@ -1,4 +1,4 @@
-import { TextField } from '@fluentui/react';
+import { Position, SpinButton, TextField, Toggle } from '@fluentui/react';
 import produce from 'immer';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -9,6 +9,9 @@ const GaugeWidgetBuilder: React.FC<IWidgetBuilderFormDataProps> = ({
     setFormData
 }) => {
     const { t } = useTranslation();
+
+    const warningBreakPointEnabled =
+        typeof formData.controlConfiguration.valueBreakPoints?.[0] === 'number';
 
     return (
         <div>
@@ -45,6 +48,45 @@ const GaugeWidgetBuilder: React.FC<IWidgetBuilderFormDataProps> = ({
                     )
                 }
             />
+            <Toggle
+                label={t('3dSceneBuilder.warningBreakPointToggleLabel')}
+                checked={warningBreakPointEnabled}
+                onText={t('on')}
+                offText={t('off')}
+                onChange={(_e, checked) => {
+                    setFormData(
+                        produce((draft) => {
+                            if (checked) {
+                                draft.controlConfiguration.valueBreakPoints = [
+                                    0
+                                ];
+                                draft.controlConfiguration.colors = ['#ffff00'];
+                            } else {
+                                draft.controlConfiguration.valueBreakPoints = [];
+                                draft.controlConfiguration.colors = [];
+                            }
+                        })
+                    );
+                }}
+            />
+            {warningBreakPointEnabled && (
+                <SpinButton
+                    label={t('3dSceneBuilder.warningBreakPointLabel')}
+                    labelPosition={Position.top}
+                    value={String(
+                        formData.controlConfiguration.valueBreakPoints[0]
+                    )}
+                    onChange={(_ev, newVal) => {
+                        setFormData(
+                            produce((draft) => {
+                                const valueBreakPoints =
+                                    draft.controlConfiguration.valueBreakPoints;
+                                valueBreakPoints[0] = Number(newVal);
+                            })
+                        );
+                    }}
+                />
+            )}
         </div>
     );
 };
