7180aa3dd78701adc7618947bc564e902ce838fd
Cory Crowley <ccrowley96@gmail.com>
2022-01-21 11:53:45 -0800

    Updated async hook testing in useAdapter to be more stable : o

--- src/Models/Hooks/__tests__/useAdapter.test.ts
+++ src/Models/Hooks/__tests__/useAdapter.test.ts
@@ -1,54 +1,41 @@
-import {
-    renderHook,
-    act,
-    RenderHookResult,
-    cleanup
-} from '@testing-library/react-hooks';
+import { renderHook, act, cleanup } from '@testing-library/react-hooks';
 import useAdapter from '../useAdapter';
 import { MockAdapter } from '../../../Adapters';
 import { AdapterResult, KeyValuePairAdapterData } from '../../Classes';
-import { IUseAdapter } from '../../Constants';
 
-const networkTimeoutMillis = 0;
-const pollingInterval = 0;
+const networkTimeoutMillis = 50;
+const pollingIntervalMillis = 50;
 
-let adapterInfo;
-let renderedHook: RenderHookResult<any, IUseAdapter<KeyValuePairAdapterData>>;
+jest.mock('../../../i18n.ts', () => ({ t: () => 'testTranslation' }));
 
-beforeEach(() => {
-    adapterInfo = {
-        adapter: new MockAdapter({ networkTimeoutMillis }), // Explicitly set network timeout period
-        id: 'test',
-        properties: ['temp', 'speed', 'pressure']
+const getUseAdapterParams: (options?: { isLongPolling?: boolean }) => any = (
+    options
+) => {
+    return {
+        adapterMethod: () => {
+            return new MockAdapter({
+                networkTimeoutMillis,
+                isDataStatic: false
+            }).getKeyValuePairs('test', ['temp', 'speed', 'pressure'], {});
+        },
+        refetchDependencies: [],
+        ...(options?.isLongPolling
+            ? {
+                  isLongPolling: true,
+                  pollingIntervalMillis,
+                  pulseTimeoutMillis: pollingIntervalMillis
+              }
+            : {})
     };
-});
+};
 
-afterEach(() => {
-    cleanup();
-});
+afterEach(cleanup);
 
 describe('Basic useAdapter tests', () => {
-    // Call hook with minimal props
-    beforeEach(() => {
-        act(() => {
-            renderedHook = renderHook(() =>
-                useAdapter<KeyValuePairAdapterData>({
-                    adapterMethod: () =>
-                        adapterInfo.adapter.getKeyValuePairs(
-                            adapterInfo.id,
-                            adapterInfo.properties
-                        ),
-                    refetchDependencies: adapterInfo.properties,
-                    isAdapterCalledOnMount: true
-                })
-            );
-        });
-    });
-
     test('useAdapter return type is correct on initial call', () => {
-        const {
-            result: { current }
-        } = renderedHook;
+        const { result } = renderHook(() => useAdapter(getUseAdapterParams()));
+        const current = result.current;
+
         expect(current.adapterResult).toEqual(
             new AdapterResult<KeyValuePairAdapterData>({
                 result: null,
@@ -59,155 +46,140 @@ describe('Basic useAdapter tests', () => {
         expect(current.pulse).toBe(false);
     });
 
-    test('loading state is truthy while loading, then false once data loaded', async () => {
-        expect(renderedHook.result.current.isLoading).toBe(true);
-        await renderedHook.waitForValueToChange(
-            () => renderedHook.result.current.adapterResult
+    test('loading state is truthy while loading, then false once data loaded', async (done) => {
+        const { result, waitForNextUpdate } = renderHook(() =>
+            useAdapter(getUseAdapterParams())
         );
-        expect(renderedHook.result.current.isLoading).toBe(false);
+
+        expect(result.current.isLoading).toBe(true);
+        await waitForNextUpdate();
+        expect(result.current.isLoading).toBe(false);
+        done();
     });
 
-    test('rendered component with useAdapter hook can safely unmount during adapter data fetch', async () => {
+    test('rendered component with useAdapter hook can safely unmount during adapter data fetch', async (done) => {
+        const { unmount } = renderHook(() => useAdapter(getUseAdapterParams()));
         act(() => {
-            renderedHook.unmount();
+            unmount();
         });
+        done();
     });
 
-    test('manual adapter data refetch', async () => {
-        // Wait for initial data fetch to complete
-        await renderedHook.waitForValueToChange(
-            () => renderedHook.result.current.isLoading
-        );
+    test('manual adapter data refetch fetches new data', async (done) => {
+        const {
+            result,
+            waitForNextUpdate,
+            waitForValueToChange
+        } = renderHook(() => useAdapter(getUseAdapterParams()));
 
-        expect(renderedHook.result.current.isLoading).toBe(false);
+        // Wait for initial data fetch to complete
+        await waitForNextUpdate();
 
         // Save intial data fetched
-        const dataFetched =
-            renderedHook.result.current.adapterResult.result.data;
+        const dataFetched = result.current.adapterResult.result.data;
 
         // Trigger manual adapter call
         act(() => {
-            renderedHook.result.current.callAdapter();
+            result.current.callAdapter();
         });
 
-        expect(renderedHook.result.current.isLoading).toBe(true);
-
-        // Wait for manual data fetch (callAdapter) to complete
-        await renderedHook.waitForValueToChange(
-            () => renderedHook.result.current.isLoading
-        );
+        // Wait for refetch to complete
+        await waitForValueToChange(() => result.current.isLoading);
 
         // Verify new data is present
-        expect(renderedHook.result.current.isLoading).toBe(false);
-        expect(
-            renderedHook.result.current.adapterResult.result.data
-        ).not.toEqual(dataFetched);
+        expect(result.current.adapterResult.result.data).not.toEqual(
+            dataFetched
+        );
+        done();
     });
 });
 
 describe('Long polling useAdapter tests', () => {
-    // Call hook with long polling props
-    beforeEach(() => {
-        act(() => {
-            renderedHook = renderHook(() =>
-                useAdapter<KeyValuePairAdapterData>({
-                    adapterMethod: () =>
-                        adapterInfo.adapter.getKeyValuePairs(
-                            adapterInfo.id,
-                            adapterInfo.properties
-                        ),
-                    refetchDependencies: adapterInfo.properties,
-                    isAdapterCalledOnMount: true,
-                    isLongPolling: true,
-                    pollingIntervalMillis: pollingInterval,
-                    pulseTimeoutMillis: pollingInterval
-                })
-            );
-        });
-    });
+    test('Long polling activated, pulse state, and long polling toggle', async (done) => {
+        const {
+            result,
+            waitForNextUpdate,
+            waitForValueToChange,
+            unmount
+        } = renderHook(() =>
+            useAdapter(getUseAdapterParams({ isLongPolling: true }))
+        );
 
-    test('Long polling activated, pulse state, and long polling toggle', async () => {
         // Test initial hook state on mount
-        expect(renderedHook.result.current.adapterResult).toEqual(
+        expect(result.current.adapterResult).toEqual(
             new AdapterResult<KeyValuePairAdapterData>({
                 result: null,
                 errorInfo: null
             })
         );
-        expect(renderedHook.result.current.isLoading).toBe(true);
-        expect(renderedHook.result.current.pulse).toBe(false);
-        expect(renderedHook.result.current.isLongPolling).toBe(true);
+        expect(result.current.pulse).toBe(false);
+        expect(result.current.isLongPolling).toBe(true);
 
         // Wait for loading state to change
-        await renderedHook.waitForValueToChange(
-            () => renderedHook.result.current.isLoading
-        );
+        await waitForValueToChange(() => result.current.isLoading);
 
         // Test state once data has loaded and save data fetched into variable
-        expect(renderedHook.result.current.adapterResult.hasNoData()).toBe(
-            false
-        );
-        expect(renderedHook.result.current.isLoading).toBe(false);
-        expect(renderedHook.result.current.pulse).toBe(true);
-        const dataFetched =
-            renderedHook.result.current.adapterResult.result.data;
+        expect(result.current.adapterResult.hasNoData()).toBe(false);
+        expect(result.current.pulse).toBe(true);
+        const dataFetched = result.current.adapterResult.result.data;
 
         // Wait for first long poll completion.  Data should be updated.
-        await renderedHook.waitForValueToChange(
-            () => renderedHook.result.current.adapterResult.result.data
-        );
+        await waitForNextUpdate();
 
         expect(dataFetched).not.toEqual(
-            renderedHook.result.current.adapterResult.result.data
+            result.current.adapterResult.result.data
         );
 
         // Toggle off long polling
         act(() => {
-            renderedHook.result.current.setIsLongPolling(false);
+            result.current.setIsLongPolling(false);
         });
 
-        expect(renderedHook.result.current.isLongPolling).toBe(false);
+        expect(result.current.isLongPolling).toBe(false);
+        unmount();
+        done();
     });
 });
 
 describe('Dependency change testing', () => {
-    test('Changing hook dependencies triggers data refetch', async () => {
-        const id = 'test1';
-        let properties = ['temp', 'speed', 'pressure'];
+    test('Changing hook dependencies triggers data refetch', async (done) => {
+        let refetchDeps = ['temp', 'press', 'vol'];
 
-        act(() => {
-            // Call hook with initial id and properties
-            renderedHook = renderHook(() =>
-                useAdapter<KeyValuePairAdapterData>({
-                    adapterMethod: () =>
-                        adapterInfo.adapter.getKeyValuePairs(id, properties),
-                    refetchDependencies: properties
-                })
-            );
-        });
-
-        await renderedHook.waitForValueToChange(
-            () => renderedHook.result.current.isLoading
+        const { result, rerender, waitForValueToChange } = renderHook(() =>
+            useAdapter({
+                adapterMethod: () =>
+                    new MockAdapter({
+                        networkTimeoutMillis,
+                        isDataStatic: false
+                    }).getKeyValuePairs(
+                        'test',
+                        ['temp', 'speed', 'pressure'],
+                        {}
+                    ),
+                refetchDependencies: refetchDeps,
+                isAdapterCalledOnMount: true
+            })
         );
 
-        const dataFetched =
-            renderedHook.result.current.adapterResult.result.data;
+        // Wait for original data to load
+        await waitForValueToChange(() => result.current.isLoading);
 
-        // Change properties
-        properties = ['altitude', 'acceleration', 'direction'];
+        const dataFetched = result.current.adapterResult.result.data;
+        expect(dataFetched).not.toBe(null);
 
+        // Change refetch deps
         act(() => {
-            renderedHook.rerender();
+            refetchDeps = ['height', 'weight', 'rad'];
         });
-        // Test for refetch and new data
-        expect(renderedHook.result.current.isLoading).toBe(true);
 
-        await renderedHook.waitForValueToChange(
-            () => renderedHook.result.current.isLoading
-        );
+        rerender();
+
+        // Wait for data to load
+        await waitForValueToChange(() => result.current.isLoading);
 
         expect(dataFetched).not.toEqual(
-            renderedHook.result.current.adapterResult.result.data
+            result.current.adapterResult.result.data
         );
+        done();
     });
 });
