27c1c79983543ee56b5198da92c5b8c376c7a475
Mitch Snyder <msnyder@microsoft.com>
2022-02-10 11:16:20 -0800

    Fix lint

--- src/Utilities/StoryUtilities.ts
+++ src/Utilities/StoryUtilities.ts
@@ -7,8 +7,8 @@
  * @param ms number of milliseconds to wait
  * @returns a promise that resolves after x milliseconds
  */
- export function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+export function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 /**
  * Function to wait for a period of time and resolves when that time has ellapsed
@@ -16,5 +16,5 @@
  * @returns a promise that resolves after x milliseconds
  */
 export async function waitForFirstRender() {
-  return sleep(1);
+    return sleep(1);
 }
