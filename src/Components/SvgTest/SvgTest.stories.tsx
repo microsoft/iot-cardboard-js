59b185f7f3c20aed6ad56e6f732502ab34ecc3af
Cory Crowley <ccrowley96@gmail.com>
2022-02-28 16:31:27 -0800

    Added chromatic delay to svgtest stories

--- src/Components/SvgTest/SvgTest.stories.tsx
+++ src/Components/SvgTest/SvgTest.stories.tsx
@@ -3,7 +3,11 @@ import SvgTest from './SvgTest';
 
 export default {
     title: 'Components/SvgTest',
-    component: SvgTest
+    component: SvgTest,
+    parameters: {
+        // Sets a delay for the component's stories
+        chromatic: { delay: 500 }
+    }
 };
 
 export const TestLoadingSvg = () => <SvgTest />;
