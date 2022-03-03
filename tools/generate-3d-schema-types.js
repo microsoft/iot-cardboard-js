a4300710125722cf0f4288e483483bb57a928fee
Cory Crowley <ccrowley96@gmail.com>
2022-03-02 21:45:07 -0800

    added path.resolve to type generating script to make more robust

--- tools/generate-3d-schema-types.js
+++ tools/generate-3d-schema-types.js
@@ -2,22 +2,22 @@ require('colors');
 const fs = require('fs');
 const generator = require('json-schema-to-typescript');
 const prettierConfig = require('../.prettierrc.json');
+const path = require('path');
 
-const schemaVersion = 'v1.0.0';
-const schemaPath =
-    './schemas/3DScenesConfiguration/v1.0.0/3DScenesConfiguration.schema.json';
-const generatedTypesOutputFolder = './src/Models/Types/Generated';
+const schemaVersion = 'v1.1.0';
+
+const schemaPath = path.resolve(
+    __dirname,
+    '../schemas/3DScenesConfiguration/v1.0.0/3DScenesConfiguration.schema.json'
+);
+
+const generatedTypesOutputFolder = path.resolve(
+    __dirname,
+    '../src/Models/Types/Generated'
+);
 const generatedTypesFileName = `3DScenesConfiguration-${schemaVersion}.d.ts`;
 
 async function generate() {
-    // Verify script is run from root (ie schemas folder exists)
-    if (!fs.existsSync(schemaPath.slice(0, schemaPath.lastIndexOf('/')))) {
-        console.error(
-            `Schema path: ${schemaPath} not found.  Ensure this script is run from the root of the project & that the schema exists.`
-                .red
-        );
-        process.exit(1);
-    }
     if (!fs.existsSync(generatedTypesOutputFolder)) {
         console.log(
             `Creating 3DSceneConfiguration types folder: ${generatedTypesOutputFolder}`
