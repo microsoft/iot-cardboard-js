856657b85f3c93aa5c9677b98942eaadd732f6af
Cory Crowley <ccrowley96@gmail.com>
2022-03-02 21:32:29 -0800

    Ensure catastrophic errors are shifted into errors array

--- src/Models/Classes/AdapterResult.ts
+++ src/Models/Classes/AdapterResult.ts
@@ -27,16 +27,8 @@ class AdapterResult<T extends IAdapterData> {
 
     /** Returns error array if errors are present.  If no errors present, returns null */
     getErrors() {
-        if (
-            (this.errorInfo?.errors && this.errorInfo?.errors.length > 0) ||
-            this.errorInfo?.catastrophicError
-        ) {
-            return [
-                ...(this.errorInfo.errors || []),
-                ...(this?.errorInfo?.catastrophicError
-                    ? [this.errorInfo.catastrophicError]
-                    : undefined)
-            ];
+        if (this.errorInfo?.errors && this.errorInfo?.errors.length > 0) {
+            return this.errorInfo.errors;
         }
         return null;
     }
