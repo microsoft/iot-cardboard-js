a3297b7ce5f49cf996f564c1f2a63ea7aaad55ec
JoseZarmada <jose@zarmada.com>
2022-03-23 13:08:04 -0600

    ADTS-77 Rolling Back code in JsonPreviewer

--- src/Components/JsonPreview/JsonPreview.stories.tsx
+++ src/Components/JsonPreview/JsonPreview.stories.tsx
@@ -12,35 +12,18 @@ export default {
 export const JsonPreview = (_args, { globals: { theme } }) => {
     const { t } = useTranslation();
     const [isPreviewOpen, setIsPreviewOpen] = useState(true);
-    const [jsonHandler, setJsonHandler] = useState(json);
-
-    const cleanJson = {
-        id: 'someId',
-        extends: 'someExtends'
-    };
-
-    const handleJsonChange = (newJson) => {
-        setJsonHandler(newJson);
-    };
-
     return (
         <div>
             <DefaultButton onClick={() => setIsPreviewOpen((prev) => !prev)}>
                 {t('modelSearch.modelListItemPreview')}
             </DefaultButton>
-            <label>This is the Button</label>
             <JsonPreviewView
                 isOpen={isPreviewOpen}
                 onDismiss={() => setIsPreviewOpen(false)}
-                json={jsonHandler}
+                json={json}
                 modalTitle={'EV Charging Station'}
                 theme={theme}
-                handleJsonChange={handleJsonChange}
             />
-            <DefaultButton onClick={() => setJsonHandler(cleanJson)}>
-                CleanButton
-            </DefaultButton>
-            <label>{JSON.stringify(jsonHandler, null, 2)}</label>
         </div>
     );
 };
