8ab17f1cbaa22983be1b2bbe2de7c716ba5ab4f5
Mitch Snyder <msnyder@microsoft.com>
2022-03-24 14:34:10 -0700

    Split widget tests

--- src/Components/ADT3DSceneBuilder/ADT3DSceneBuilder.WidgetsForm.stories.tsx
+++ src/Components/ADT3DSceneBuilder/ADT3DSceneBuilder.WidgetsForm.stories.tsx
@@ -11,9 +11,13 @@ import {
 } from '../../Models/Services/StoryUtilities';
 import { IADT3DSceneBuilderCardProps } from './ADT3DSceneBuilder.types';
 import { deepCopy } from '../../Models/Services/Utils';
+import {
+    WidgetsListAddDialogShow,
+    WidgetsListMore
+} from './ADT3DSceneBuilder.WidgetsList.stories';
 
 export default {
-    title: 'Components/ADT3DSceneBuilder/Widgets',
+    title: 'Components/ADT3DSceneBuilder/WidgetsForm',
     component: ADT3DSceneBuilder
 };
 
@@ -45,87 +49,6 @@ const Template: SceneBuilderStory = (
             />
         </div>
     );
-};
-
-export const WidgetsList = Template.bind({});
-WidgetsList.play = async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    // Finds the behaviors tab
-    const behaviorsTabButton = (await canvas.findAllByRole('tab'))[1];
-    await userEvent.click(behaviorsTabButton);
-
-    // click the behavior
-    const listItem = await canvas.findByTestId(
-        'cardboard-list-item-behaviors-in-scene-0'
-    );
-    await userEvent.click(listItem);
-
-    // click one of the items in the elements list
-    const elementListItem = await canvas.findByText('box1');
-    await userEvent.click(elementListItem);
-
-    // click one of the items in the list
-    // Finds the tabs and clicks the first one
-    const tab = await canvas.findAllByRole('tab');
-    await userEvent.click(tab[3]);
-};
-
-export const WidgetsListEmpty = Template.bind({});
-WidgetsListEmpty.play = async ({ canvasElement }) => {
-    await WidgetsList.play({ canvasElement });
-
-    // remove the stock data
-    const canvas = within(canvasElement);
-    const moreButton1 = await canvas.findByTestId(
-        `context-menu-widgets-in-behavior-0-moreMenu`
-    );
-    await userEvent.click(moreButton1);
-    await clickOverFlowMenuItem(
-        await findOverflowMenuItem('removeWidgetOverflow')
-    );
-    const moreButton2 = await canvas.findByTestId(
-        `context-menu-widgets-in-behavior-0-moreMenu`
-    );
-    await userEvent.click(moreButton2);
-    await clickOverFlowMenuItem(
-        await findOverflowMenuItem('removeWidgetOverflow')
-    );
-    const moreButton3 = await canvas.findByTestId(
-        `context-menu-widgets-in-behavior-0-moreMenu`
-    );
-    await userEvent.click(moreButton3);
-    await clickOverFlowMenuItem(
-        await findOverflowMenuItem('removeWidgetOverflow')
-    );
-};
-
-export const WidgetsListMore = Template.bind({});
-WidgetsListMore.play = async ({ canvasElement, listItemIndex = 0 }) => {
-    await WidgetsList.play({ canvasElement });
-    // click one of the items in the list
-    const canvas = within(canvasElement);
-    // Finds the tabs and clicks the first one
-    const moreButton = await canvas.findByTestId(
-        `context-menu-widgets-in-behavior-${listItemIndex}-moreMenu`
-    );
-    await userEvent.click(moreButton);
-};
-
-export const WidgetsListMoreRemove = Template.bind({});
-WidgetsListMoreRemove.play = async ({ canvasElement }) => {
-    await WidgetsListMore.play({ canvasElement });
-    // Click the remove button in the overflow
-    const removeButton = await findOverflowMenuItem('removeWidgetOverflow');
-    await clickOverFlowMenuItem(removeButton);
-};
-
-export const WidgetsListAddDialogShow = Template.bind({});
-WidgetsListAddDialogShow.play = async ({ canvasElement }) => {
-    await WidgetsList.play({ canvasElement });
-    // Click the remove button in the overflow
-    const canvas = within(canvasElement);
-    const addButton = await canvas.findByTestId('widgetForm-addWidget');
-    await userEvent.click(addButton);
 };
 
 export const WidgetsFormEditLink = Template.bind({});
