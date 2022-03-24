7e97b5bfecfe2f46073b1f8cb63f067f5e1df6a9
Matt Darsney <matdar@microsoft.com>
2022-03-23 19:17:08 -0700

    just revved a couple hex values in kraken theme

--- src/Theming/FluentComponentStyles/PrimaryButton.styles.ts
+++ src/Theming/FluentComponentStyles/PrimaryButton.styles.ts
@@ -1,28 +1,34 @@
 import { IButtonStyles, ITheme } from '@fluentui/react';
 import { Theme } from '../../Models/Constants/Enums';
-import { customOverrides } from '../Palettes';
 
 // Overrides PrimaryButton styles
 export const getPrimaryButtonStyles = (
     themeSetting: Theme,
-    _theme: ITheme
+    theme: ITheme
 ): Partial<IButtonStyles> => {
     // Adds box shadow on light theme
-    const boxShadow =
-        themeSetting === Theme.Light
-            ? `0 1px 3px 0 rgba(0, 0, 0, .12)`
-            : '0 1px 3px 0 rgba(255,255,255,.12)';
+
+    const customOverrides = {
+        boxShadow:
+            themeSetting === Theme.Light
+                ? `0 1px 3px 0 rgba(0, 0, 0, .12)`
+                : '0 1px 3px 0 rgba(255,255,255,.12)',
+        primaryButtonTextColor:
+            themeSetting === Theme.Kraken ? theme.palette.white : '#F2F3F4',
+        primaryButtonTextColorDisabled:
+            themeSetting === Theme.Kraken ? theme.palette.white : '#F2F3F4'
+    };
 
     return {
         root: {
-            boxShadow,
-            color: customOverrides.primaryButtonTextColor // force in all themes
+            boxShadow: customOverrides.boxShadow,
+            color: customOverrides.primaryButtonTextColor
         },
         rootHovered: {
-            color: customOverrides.primaryButtonTextColor // force in all themes
+            color: customOverrides.primaryButtonTextColor
         },
         rootPressed: {
-            color: customOverrides.primaryButtonTextColor // force in all themes
+            color: customOverrides.primaryButtonTextColor
         },
         rootDisabled: {
             color: customOverrides.primaryButtonTextColorDisabled
