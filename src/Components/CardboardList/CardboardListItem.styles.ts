8615ab2d1c23fbceab64887e3f31c916f1927311
Mitch Snyder <msnyder@microsoft.com>
2022-03-30 13:11:37 -0700

    Fix nested button error

--- src/Components/CardboardList/CardboardListItem.styles.ts
+++ src/Components/CardboardList/CardboardListItem.styles.ts
@@ -18,64 +18,92 @@ const classNames = {
     endIcon: `${classPrefix}-end-icon`,
     icon: `${classPrefix}-icon`,
     menuIcon: `${classPrefix}-menu-icon`,
+    menuPlaceholder: `${classPrefix}-menu-placeholder`,
     textContainer: `${classPrefix}-text-container`,
     primaryText: `${classPrefix}-primary-text`,
+    root: `${classPrefix}-root`,
     secondaryText: `${classPrefix}-secondary-text`,
     separator: `${classPrefix}-separator`
 };
-export const getStyles = memoizeFunction((theme: Theme) => {
-    const ellipseStyles = {
-        overflow: 'hidden',
-        whiteSpace: 'nowrap',
-        textOverflow: 'ellipsis'
-    };
-    return mergeStyleSets({
-        checkbox: [classNames.checkbox, { marginRight: 8 } as IStyle],
-        endIcon: [classNames.endIcon, { marginLeft: 8 } as IStyle],
-        icon: [
-            classNames.icon,
-            {
-                marginRight: 8,
-                fontSize: StyleConstants.icons.size16
-            } as IStyle
-        ],
-        menuIcon: [classNames.menuIcon, { opacity: 0 } as IStyle],
-        textContainer: [
-            classNames.textContainer,
-            {
-                display: 'flex',
-                flexDirection: 'column',
-                flexGrow: 1,
-                overflow: 'hidden',
-                textAlign: 'start'
-            } as IStyle
-        ],
-        primaryText: [
-            classNames.primaryText,
-            {
-                color: theme.palette.black,
-                fontSize: FontSizes.size14,
-                ...ellipseStyles
-            } as IStyle
-        ],
-        secondaryText: [
-            classNames.secondaryText,
-            {
-                color: theme.palette.neutralSecondary,
-                fontSize: FontSizes.size12,
-                ...ellipseStyles
-            } as IStyle
-        ],
-        separator: [
-            classNames.separator,
-            {
-                backgroundColor: 'transparent',
-                padding: 0,
-                height: 1
-            } as IStyle
-        ]
-    });
-});
+export const getStyles = memoizeFunction(
+    (theme: Theme, isMenuOpen: boolean) => {
+        const ellipseStyles = {
+            overflow: 'hidden',
+            whiteSpace: 'nowrap',
+            textOverflow: 'ellipsis'
+        };
+        return mergeStyleSets({
+            checkbox: [classNames.checkbox, { marginRight: 8 } as IStyle],
+            endIcon: [classNames.endIcon, { marginLeft: 8 } as IStyle],
+            icon: [
+                classNames.icon,
+                {
+                    marginRight: 8,
+                    fontSize: StyleConstants.icons.size16
+                } as IStyle
+            ],
+            menuIcon: [
+                classNames.menuIcon,
+                { opacity: 0, position: 'absolute', right: 12 } as IStyle
+            ],
+            menuPlaceholder: [
+                classNames.menuPlaceholder,
+                { width: 32 } as IStyle
+            ],
+            textContainer: [
+                classNames.textContainer,
+                {
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flexGrow: 1,
+                    overflow: 'hidden',
+                    textAlign: 'start'
+                } as IStyle
+            ],
+            primaryText: [
+                classNames.primaryText,
+                {
+                    color: theme.palette.black,
+                    fontSize: FontSizes.size14,
+                    ...ellipseStyles
+                } as IStyle
+            ],
+            root: [
+                classNames.root,
+                {
+                    alignItems: 'center',
+                    display: 'flex',
+                    flexDirection: 'horizontal',
+                    ':hover .cb-list-item-menu-icon, :focus-within .cb-list-item-menu-icon': {
+                        opacity: 1
+                    }
+                } as IStyle,
+                // force the menu icon to show up when the menu is open since it's no longer hovered/focused
+                isMenuOpen && {
+                    '& .cb-list-item-menu-icon': {
+                        opacity: 1
+                    }
+                }
+            ],
+            secondaryText: [
+                classNames.secondaryText,
+                {
+                    color: theme.palette.neutralSecondary,
+                    fontSize: FontSizes.size12,
+                    ...ellipseStyles
+                } as IStyle
+            ],
+            separator: [
+                classNames.separator,
+                {
+                    backgroundColor: 'transparent',
+                    padding: 0,
+                    height: 1
+                } as IStyle
+            ]
+        });
+    }
+);
 export const getButtonStyles = memoizeFunction(
     (theme: Theme, customStyles?: Partial<IButtonStyles>): IButtonStyles => {
         return {
@@ -91,11 +119,11 @@ export const getButtonStyles = memoizeFunction(
                 ...(customStyles?.root as IRawStyle)
             },
             rootFocused: {
-                backgroundColor: theme.palette.neutralLighter,
+                // backgroundColor: theme.palette.neutralLighter,
                 ...(customStyles?.rootFocused as IRawStyle)
             },
             rootHovered: {
-                backgroundColor: theme.palette.neutralLighter,
+                // backgroundColor: theme.palette.neutralLighter,
                 ...(customStyles?.rootHovered as IRawStyle)
             },
             flexContainer: {
