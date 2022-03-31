533ad5b1ba532b6339e4bd6465d6b39faeda5462
JoseZarmada <jose@zarmada.com>
2022-03-31 13:26:05 -0600

    ADTS-85 Changes for OATGraphViewer

---
+++ src/Components/OATGraphViewer/Internal/OATGraphCustomNode.tsx
@@ -0,0 +1,95 @@
+import React, { memo, useCallback, useState } from 'react';
+import { useTheme, Separator } from '@fluentui/react';
+
+import { Handle } from 'react-flow-renderer';
+
+export default memo(({ data, isConnectable }) => {
+    const [nameEditor, setNameEditor] = useState(false);
+    const [nameText, setNameText] = useState(data.name);
+    const [idEditor, setIdEditor] = useState(false);
+    const [idText, setIdText] = useState(data.id);
+    const theme = useTheme();
+
+    const onNameChange = useCallback((evt) => {
+        data.name = evt.target.value;
+        setNameText(evt.target.value);
+    }, []);
+
+    const onNameClick = useCallback(() => {
+        setNameEditor(true);
+    }, []);
+
+    const onNameBlur = useCallback(() => {
+        setNameEditor(false);
+    }, []);
+
+    const onIdChange = useCallback((evt) => {
+        data.id = evt.target.value;
+        setIdText(evt.target.value);
+    }, []);
+
+    const onIdClick = useCallback(() => {
+        setIdEditor(true);
+    }, []);
+
+    const onIdBlur = useCallback(() => {
+        setIdEditor(false);
+    }, []);
+
+    return (
+        <>
+            <Handle
+                type="target"
+                position="top"
+                style={{ background: theme.semanticColors.variantBorder }}
+                isConnectable={isConnectable}
+            />
+            <div>
+                <div>
+                    Name:{' '}
+                    {!nameEditor && (
+                        <strong onClick={onNameClick}>{data.name}</strong>
+                    )}
+                    {nameEditor && (
+                        <input
+                            id="text"
+                            name="text"
+                            onChange={onNameChange}
+                            value={nameText}
+                            onBlur={onNameBlur}
+                            autoFocus
+                        />
+                    )}
+                </div>
+                <div>
+                    Id:{' '}
+                    {!idEditor && (
+                        <strong onClick={onIdClick}>{data.id}</strong>
+                    )}
+                    {idEditor && (
+                        <input
+                            id="text"
+                            name="text"
+                            onChange={onIdChange}
+                            value={idText}
+                            onBlur={onIdBlur}
+                            autoFocus
+                        />
+                    )}
+                </div>
+                <div>
+                    Type:<strong>{data.type}</strong>
+                </div>
+            </div>
+            <Handle
+                type="source"
+                position="bottom"
+                id="Relationship"
+                style={{
+                    background: theme.semanticColors.variantBorder
+                }}
+                isConnectable={isConnectable}
+            />
+        </>
+    );
+});
--- src/Components/OATGraphViewer/OATGraphViewer.tsx
+++ src/Components/OATGraphViewer/OATGraphViewer.tsx
@@ -1,13 +1,126 @@
-import React from 'react';
-import { useTheme } from '@fluentui/react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
+import { useTheme, PrimaryButton } from '@fluentui/react';
+import ReactFlow, {
+    ReactFlowProvider,
+    addEdge,
+    MiniMap,
+    Controls,
+    Background,
+    removeElements
+} from 'react-flow-renderer';
+import { useTranslation } from 'react-i18next';
 import BaseComponent from '../BaseComponent/BaseComponent';
+import OATGraphCustomNode from './Internal/OATGraphCustomNode';
+import OATGraphCustomEdge from './Internal/OATGraphCustomEdge';
+
+import './OATGraphViewer.scss';
 
 const OATGraphViewer = () => {
+    const { t } = useTranslation();
     const theme = useTheme();
+    const reactFlowWrapper = useRef(null);
+    const [storedElements, setStoredElements] = useState(
+        JSON.parse(localStorage.getItem('elements'))
+    );
+    const [elements, setElements] = useState(
+        storedElements === null ? [] : storedElements
+    );
+    const [idClass, setIdClass] = useState('dtmi:com:example:');
+    const [newModelId, setNewModelId] = useState(0);
+
+    useEffect(() => {
+        let nextModelId = newModelId;
+        let index = 0;
+        while (index !== -1) {
+            index = elements.findIndex(
+                (element) => element.id === `${idClass}model${nextModelId}`
+            );
+            if (index === -1) {
+                setNewModelId(nextModelId);
+            }
+            nextModelId++;
+        }
+        localStorage.setItem('elements', JSON.stringify(elements));
+    }, [elements]);
+
+    const onConnect = (evt) => {
+        const params = {
+            source: evt.source,
+            target: evt.target,
+            label: '',
+            arrowHeadType: 'arrowclosed',
+            type: 'RelationShipEdge',
+            data: {
+                name: ''
+            }
+        };
+        setElements((els) => addEdge(params, els));
+    };
+
+    const nodeTypes = useMemo(() => ({ Interface: OATGraphCustomNode }), []);
+
+    const edgeTypes = useMemo(
+        () => ({ RelationShipEdge: OATGraphCustomEdge }),
+        []
+    );
+
+    const onElementsRemove = (elementsToRemove) =>
+        setElements((els) => removeElements(elementsToRemove, els));
+
+    const onLoad = (_reactFlowInstance) => {
+        _reactFlowInstance.fitView();
+    };
+
+    const onNewModelClick = () => {
+        const name = `Model${newModelId}`;
+        const id = `${idClass}model${newModelId}`;
+        const newNode = {
+            id: id,
+            type: 'Interface',
+            position: { x: 100, y: 100 },
+            data: {
+                name: name,
+                type: 'Interface',
+                id: id,
+                content: []
+            }
+        };
+        setElements((es) => es.concat(newNode));
+    };
 
     return (
         <BaseComponent theme={theme}>
-            <div></div>
+            <div>
+                <ReactFlowProvider>
+                    <div
+                        className="cb-oat-graph-viewer-container"
+                        ref={reactFlowWrapper}
+                    >
+                        <ReactFlow
+                            elements={elements}
+                            onElementsRemove={onElementsRemove}
+                            onConnect={onConnect}
+                            onLoad={onLoad}
+                            snapToGrid={true}
+                            snapGrid={[15, 15]}
+                            nodeTypes={nodeTypes}
+                            edgeTypes={edgeTypes}
+                        >
+                            <PrimaryButton
+                                className="cb-oat-graph-viewer-button"
+                                onClick={onNewModelClick}
+                                text={t('OATGraphViewer.newModel')}
+                            />
+                            <MiniMap />
+                            <Controls />
+                            <Background
+                                color={theme.semanticColors.bodyBackground}
+                                gap={16}
+                            />
+                        </ReactFlow>
+                    </div>
+                </ReactFlowProvider>
+            </div>
         </BaseComponent>
     );
 };
