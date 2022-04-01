8f8b0520b2cb948066ce9d1997f5ce26e47a3463
JoseZarmada <jose@zarmada.com>
2022-03-31 19:42:44 -0600

    ADTS-77 eslint check

--- src/Components/OATModelList/OATModelList.tsx
+++ src/Components/OATModelList/OATModelList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useTheme } from '@fluentui/react';
 import BaseComponent from '../BaseComponent/BaseComponent';
 
