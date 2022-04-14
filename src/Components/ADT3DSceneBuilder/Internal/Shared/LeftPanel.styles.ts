fa6e954c6332bf1320de54d8b1b8c5a381f41633
Cory Crowley <ccrowley96@gmail.com>
2022-04-14 11:02:24 -0700

    Updated BehaviorsModal preview container to absolute positioning relative to scene builder

--- src/Components/ADT3DSceneBuilder/Internal/Shared/LeftPanel.styles.ts
+++ src/Components/ADT3DSceneBuilder/Internal/Shared/LeftPanel.styles.ts
@@ -52,11 +52,11 @@ export const getLeftPanelStyles = memoizeFunction((theme: Theme) => {
         previewContainer: [
             classNames.previewContainer,
             {
-                position: 'fixed',
-                top: 16,
-                left: behaviorsModalPreviewContainerLeftOffset,
-                bottom: 16,
-                right: 16,
+                position: 'absolute',
+                top: 0,
+                left: 20,
+                bottom: 0,
+                right: 0,
                 zIndex: 1000,
                 pointerEvents: 'none'
             } as IStyle
