f526edfa4dc28f5ac58e1b42ea76cd89ed2d30ea
miltone92 <55605034+miltone92@users.noreply.github.com>
2022-05-04 17:03:06 -0600

    ADTS-122 Remove header

--- src/Components/OATHeader/OATHeader.tsx
+++ src/Components/OATHeader/OATHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FontIcon, TextField, ActionButton } from '@fluentui/react';
+import { FontIcon, ActionButton } from '@fluentui/react';
 import { useTranslation } from 'react-i18next';
 import { getHeaderStyles } from './OATHeader.styles';
 import JSZip from 'jszip';
@@ -39,28 +39,6 @@ const OATHeader = ({ elements }: OATHeaderProps) => {
 
     return (
         <div className={headerStyles.container}>
-            <div className={headerStyles.searchComponent}>
-                <div className={headerStyles.logo}>{t('OATHeader.title')}</div>
-                <div className={headerStyles.search}>
-                    <TextField
-                        placeholder={t('OATHeader.searchContent')}
-                    ></TextField>
-                </div>
-                <div className={headerStyles.options}>
-                    <FontIcon
-                        iconName={'Ringer'}
-                        className={headerStyles.optionIcon}
-                    />
-                    <FontIcon
-                        iconName={'Settings'}
-                        className={headerStyles.optionIcon}
-                    />
-                    <FontIcon
-                        iconName={'Help'}
-                        className={headerStyles.optionIcon}
-                    />
-                </div>
-            </div>
             <div className={headerStyles.menuComponent}>
                 <div className="cb-oat-header-model"></div>
                 <div className="cb-oat-header-menu">
