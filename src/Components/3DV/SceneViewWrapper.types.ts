9e4ffc5b4b2721563df11070e667f9fbd309d4e5
Mitch Snyder <msnyder@microsoft.com>
2022-05-09 14:02:32 -0700

    Add deeplink button

---
+++ src/Components/3DV/SceneViewWrapper.types.ts
@@ -0,0 +1,31 @@
+import {
+    ITheme,
+    IStyle,
+    IStyleFunctionOrObject,
+    IStackStyles
+} from '@fluentui/react';
+
+export interface ISceneViewWrapperProps {
+    /**
+     * Call to provide customized styling that will layer on top of the variant rules.
+     */
+    styles?: IStyleFunctionOrObject<undefined, ISceneViewWrapperStyles>;
+}
+
+export interface ISceneViewWrapperStyleProps {
+    theme: ITheme;
+}
+export interface ISceneViewWrapperStyles {
+    root: IStyle;
+    leftHeaderControlsContainer: IStyle;
+
+    /**
+     * SubComponent styles.
+     */
+    subComponentStyles?: ISceneViewWrapperSubComponentStyles;
+}
+
+export interface ISceneViewWrapperSubComponentStyles {
+    leftHeaderControlsStack: IStackStyles;
+    centerHeaderControlsStack: IStackStyles;
+}
