4f68c1d8c93bf5ac90e461c482c921eff1bc37e6
samjosephgates <54577812+samjosephgates@users.noreply.github.com>
2022-05-09 18:04:54 +0100

    feat: globe labels (#394)
    * Markers update WIP
    
    * Hide occluded labels
    
    * Null check
    
    * Comment
    
    * Bug fixes
    
    * Improve performance
    
    * Merge conflict
    
    * Add generic marker support
    
    * Updated model label, added group label with context menu
    
    * Grouped model labels WIP
    
    * Updated grouped labels
    
    * Grouped model label updates
    
    * Fix to position of group marker
    
    * Fixes to model labels on globe and updates to marker
    
    * Added extra markers
    
    * Fix to flashing markers. Added stories for simple and complex models.
    
    * Code clean up and navigation to scene from globe
    
    * Extracted styles
    
    * Updated with PR feedback
    
    * Moved markers into component
    
    * More PR comments
    
    * Added usecallback
    
    Co-authored-by: Paul Tallett <paul.tallett@microsoft.com>
    Co-authored-by: Mitch Snyder <msnyder@microsoft.com>

--- src/Components/ADT3DViewer/ADT3DViewer.tsx
+++ src/Components/ADT3DViewer/ADT3DViewer.tsx
@@ -15,7 +15,6 @@ import './ADT3DViewer.scss';
 import { withErrorBoundary } from '../../Models/Context/ErrorBoundary';
 import {
     CustomMeshItem,
-    Marker,
     SceneViewBadgeGroup,
     SceneVisual
 } from '../../Models/Classes/SceneView.types';
@@ -382,10 +381,9 @@ const ADT3DViewerBase: React.FC<IADT3DViewerProps & BaseComponentProps> = ({
         }
     }, [sceneVisuals, coloredMeshItemsProp, sceneAlerts]);
 
-    const meshClick = (_marker: Marker, mesh: { id: string }, scene: any) => {
+    const meshClick = (mesh: { id: string }, scene: any) => {
         // update the selected element on the context
         setSelectedElementId(getElementByMeshId(mesh.id)?.id);
-
         if (sceneVisuals) {
             const sceneVisual = sceneVisuals.find((sceneVisual) =>
                 sceneVisual.element.objectIDs.find((id) => id === mesh?.id)
@@ -437,7 +435,7 @@ const ADT3DViewerBase: React.FC<IADT3DViewerProps & BaseComponentProps> = ({
         }
     };
 
-    const meshHover = (_marker: Marker, mesh: { id: string }) => {
+    const meshHover = (mesh: { id: string }) => {
         if (mesh && sceneVisuals) {
             const sceneVisual = sceneVisuals.find((sceneVisual) =>
                 sceneVisual.element.objectIDs.find((id) => id === mesh?.id)
@@ -528,9 +526,8 @@ const ADT3DViewerBase: React.FC<IADT3DViewerProps & BaseComponentProps> = ({
                         coloredMeshItems: coloredMeshItems,
                         modelUrl: modelUrl,
                         onBadgeGroupHover: onBadgeGroupHover,
-                        onMeshClick: (marker, mesh, scene) =>
-                            meshClick(marker, mesh, scene),
-                        onMeshHover: (marker, mesh) => meshHover(marker, mesh),
+                        onMeshClick: meshClick,
+                        onMeshHover: meshHover,
                         outlinedMeshitems: outlinedMeshItems,
                         showHoverOnSelected: showHoverOnSelected,
                         showMeshesOnHover: showMeshesOnHover,
