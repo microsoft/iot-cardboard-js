5d8a7acf4c76f57975f2050ed42ebc7044c32ad6
Mitch Snyder <msnyder@microsoft.com>
2022-05-10 09:40:52 -0700

    Update component templates to use styled format

--- tools/templates/component/component.js
+++ tools/templates/component/component.js
@@ -1,17 +1,33 @@
 module.exports = (componentName) => ({
     content: `import React from 'react';
-import { ${componentName}Props } from './${componentName}.types';
-import { getStyles } from './${componentName}.styles';
-
-const ${componentName}: React.FC<${componentName}Props> = () => {
-    // TODO -- remove eslint overrides if not using
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const styles = getStyles();
-
-    return <div>Hello ${componentName}!</div>
-};
-
-export default ${componentName}
+    import {
+        I${componentName}Props,
+        I${componentName}StyleProps,
+        I${componentName}Styles
+    } from './${componentName}.types';
+    import { getStyles } from './${componentName}.styles';
+    import { classNamesFunction, useTheme, styled } from '@fluentui/react';
+    
+    const getClassNames = classNamesFunction<
+        I${componentName}StyleProps,
+        I${componentName}Styles
+    >();
+    
+    const ${componentName}: React.FC<I${componentName}Props> = (props) => {
+        const { styles } = props;
+        const classNames = getClassNames(styles, {
+            theme: useTheme()
+        });
+    
+        return <div className={classNames.root}>Hello ${componentName}!</div>;
+    };
+    
+    export default styled<
+        I${componentName}Props,
+        I${componentName}StyleProps,
+        I${componentName}Styles
+    >(${componentName}, getStyles);
+    
 `,
     extension: `.tsx`
 });
