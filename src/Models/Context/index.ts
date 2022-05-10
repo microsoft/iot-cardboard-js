c385ca0b7784ac6ce50f1df6a6373b884ef6b1d5
Mitch Snyder <msnyder@microsoft.com>
2022-05-10 11:12:13 -0700

    Update types

--- src/Models/Context/index.ts
+++ src/Models/Context/index.ts
@@ -1,5 +1,8 @@
 export { DeeplinkContextProvider } from './DeeplinkContext/DeeplinkContext';
-export { IPublicDeeplink } from './DeeplinkContext/DeeplinkContext.types';
+export {
+    IDeeplinkContextProviderProps,
+    IPublicDeeplink
+} from './DeeplinkContext/DeeplinkContext.types';
 
 export { ConsumerDeeplinkContextProvider } from './ConsumerDeeplinkContext/ConsumerDeeplinkContext';
 export { IConsumerDeeplinkContextProviderProps } from './ConsumerDeeplinkContext/ConsumerDeeplinkContext.types';
