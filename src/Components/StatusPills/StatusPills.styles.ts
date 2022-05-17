ed522ec2b7db39a18a35748fa5c5d6ba5350f0d2
pasanchMSFT <55852250+pasanchMSFT@users.noreply.github.com>
2022-05-16 18:04:39 -0700

    feat: Moved status bar position in behavior modals (#428)
    * Moved status bar in behavior modals
    
    * Added status pills
    
    * Fix lint

---
+++ src/Components/StatusPills/StatusPills.styles.ts
@@ -0,0 +1,47 @@
+import { mergeStyleSets } from '@fluentui/react';
+import { StatusPillsContainerWidth } from './StatusPills.types';
+
+export const classPrefix = 'cb-statuspills';
+const classNames = {
+    statusColorPill: `${classPrefix}-status-color-pill`
+};
+
+export const getStyles = (width: StatusPillsContainerWidth) => {
+    let rootStyle = null;
+    switch (width) {
+        case 'compact':
+            rootStyle = {
+                margin: '0px 8px 0px 0px'
+            };
+            break;
+        case 'wide':
+            rootStyle = {
+                width: 40
+            };
+            break;
+        default:
+            rootStyle = {
+                margin: '0px 8px 0px 0px'
+            };
+    }
+
+    return {
+        root: rootStyle
+    };
+};
+
+export const getPillStyles = (statusColor: string) => {
+    return mergeStyleSets({
+        statusColorPill: [
+            classNames.statusColorPill,
+            {
+                boxShadow: `0px 0px 4px ${statusColor}`,
+                background: statusColor,
+                width: 3,
+                height: 12,
+                borderRadius: 5,
+                margin: '0px 1px'
+            }
+        ]
+    });
+};
