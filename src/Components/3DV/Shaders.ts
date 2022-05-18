370819446e21b0635443fc2d1937357347603486
Cory Boyle <coryboyle@microsoft.com>
2022-05-18 16:40:24 -0400

    Removed commented out lines from Shaders

--- src/Components/3DV/Shaders.ts
+++ src/Components/3DV/Shaders.ts
@@ -64,65 +64,12 @@ export function makeStandardMaterial(
     if (isTransparent) {
         material.specularPower = 0;
         material.disableLighting = true;
-        //material.alphaMode = 0;
-        //material.diffuseColor = baseColor3;
-        //material.ambientColor = BABYLON.Color3.White();
         material.emissiveColor = baseColor3.scale(bgLuminanceRatio);
-        //material.needAlphaTesting = () => material.opacityTexture !== null;
-        //material.
     } else {
         material.emissiveColor = baseColor3;
         material.useEmissiveAsIllumination = true;
-        // material.specularPower = 1000;
-        // material.roughness = 1000;
         material.disableLighting = true;
-        // material.emissiveFresnelParameters = new BABYLON.FresnelParameters();
-        // material.emissiveFresnelParameters.leftColor = fresnelColor3;
-        // material.emissiveFresnelParameters.rightColor = baseColor3;
-        // material.emissiveFresnelParameters.power = 2;
-        // material.emissiveFresnelParameters.bias = 0.2;
     }
-
-    // //If translucent, set emissive settings
-    // if (lightingStyle >= 1) {
-    //     material.disableLighting = true;
-    //     material.specularPower = 0;
-    //     material.roughness = 100;
-    //     material.emissiveColor = BABYLON.Color3.White();
-
-    //     material.emissiveFresnelParameters = new BABYLON.FresnelParameters();
-    //     material.emissiveFresnelParameters.leftColor = fresnelColor3;
-    //     material.emissiveFresnelParameters.rightColor = baseColor3;
-    //     material.emissiveFresnelParameters.power = 2;
-    //     material.emissiveFresnelParameters.bias = 0.2;
-
-    //     material.useEmissiveAsIllumination = true;
-    // }
-
-    // //diffuse fresnel
-    // if (fresnelColor && lightingStyle == 0) {
-    //     material.diffuseFresnelParameters = new BABYLON.FresnelParameters();
-    //     material.diffuseFresnelParameters.leftColor = fresnelColor3;
-    //     material.diffuseFresnelParameters.rightColor = baseColor3;
-    //     material.diffuseFresnelParameters.bias = 4.0;
-    //     material.diffuseFresnelParameters.power = 4.5;
-    // }
-
-    // //Alpha and alphamode
-    // material.backFaceCulling = baseColor.a >= 1;
-    // material.alpha = baseColor.a;
-    // material.alphaMode = selectAlphaMode(baseColor.a);
-
-    // //Reflection map
-    // if (reflectionTexture) {
-    //     material.reflectionTexture = reflectionTexture;
-    //     material.useReflectionOverAlpha = lightingStyle !== 0;
-    //     material.reflectionFresnelParameters = new BABYLON.FresnelParameters();
-    //     material.reflectionFresnelParameters.leftColor = BABYLON.Color3.White();
-    //     material.reflectionFresnelParameters.rightColor = BABYLON.Color3.Black();
-    //     material.reflectionFresnelParameters.bias = 0.3;
-    //     material.reflectionFresnelParameters.power = 0.2;
-    // }
 
     return material;
 }
