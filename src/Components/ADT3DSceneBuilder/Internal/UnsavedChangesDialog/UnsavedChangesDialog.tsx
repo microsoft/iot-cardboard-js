bc40496ba79817bc9e4e9b376177b009e08f447a
Sam Gates <samgates@microsoft.com>
2022-05-19 18:19:30 +0100

    Added unsaved chnages dialog and hooked up to behaviors. Added overflow to behaviors form pivot

---
+++ src/Components/ADT3DSceneBuilder/Internal/UnsavedChangesDialog/UnsavedChangesDialog.tsx
@@ -0,0 +1,79 @@
+import {
+    DefaultButton,
+    Dialog,
+    DialogFooter,
+    DialogType,
+    IDialogContentProps,
+    IDialogStyles,
+    IModalProps,
+    PrimaryButton
+} from '@fluentui/react';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+interface Props {
+    title?: string;
+    message?: string;
+    isOpen: boolean;
+    onClose: () => any;
+    onConfirmDiscard: () => any;
+}
+
+const UnsavedChangesDialog: React.FC<Props> = ({
+    title,
+    message,
+    isOpen,
+    onClose,
+    onConfirmDiscard
+}) => {
+    const { t } = useTranslation();
+
+    const unsavedChangesDialogProps: IDialogContentProps = {
+        type: DialogType.normal,
+        title: title ?? t('discardChanges'),
+        closeButtonAriaLabel: t('close'),
+        subText: message ?? t('discardChangesDesc')
+    };
+
+    const unsavedChangesDialogStyles: Partial<IDialogStyles> = {
+        main: {
+            maxWidth: 450,
+            minHeight: 165
+        }
+    };
+
+    const unsavedChangesModalProps: IModalProps = {
+        isBlocking: false,
+        styles: unsavedChangesDialogStyles
+    };
+
+    return (
+        <Dialog
+            hidden={!isOpen}
+            onDismiss={() => {
+                onClose();
+            }}
+            dialogContentProps={unsavedChangesDialogProps}
+            modalProps={unsavedChangesModalProps}
+        >
+            <DialogFooter>
+                <DefaultButton
+                    onClick={() => {
+                        onClose();
+                    }}
+                    text={t('cancel')}
+                />
+                <PrimaryButton
+                    onClick={() => {
+                        onConfirmDiscard();
+                        onClose();
+                    }}
+                    text={t('discard')}
+                    autoFocus
+                />
+            </DialogFooter>
+        </Dialog>
+    );
+};
+
+export default UnsavedChangesDialog;
