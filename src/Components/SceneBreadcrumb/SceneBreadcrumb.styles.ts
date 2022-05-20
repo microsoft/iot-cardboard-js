cb75d709c127c2febabfcb5413e9a06c24c90b71
Patricio Sanchez <pasanch@microsoft.com>
2022-05-20 12:49:37 -0700

    Background styling changes to avoid themeing issues

--- src/Components/SceneBreadcrumb/SceneBreadcrumb.styles.ts
+++ src/Components/SceneBreadcrumb/SceneBreadcrumb.styles.ts
@@ -1,8 +1,15 @@
-import { FontSizes, IBreadcrumbStyles } from '@fluentui/react';
+import { FontSizes, IBreadcrumbStyles, Theme } from '@fluentui/react';
 
-export const breadcrumbStyles: Partial<IBreadcrumbStyles> = {
-    root: { marginTop: 0 },
-    item: { fontSize: FontSizes.size14 },
-    listItem: { fontSize: FontSizes.size14 },
-    itemLink: { fontSize: FontSizes.size14 }
-};
+export function breadcrumbStyles(theme: Theme): Partial<IBreadcrumbStyles> {
+    return {
+        root: { marginTop: 0 },
+        item: { fontSize: FontSizes.size14 },
+        listItem: {
+            fontSize: FontSizes.size14,
+            background: theme.palette.white,
+            // Added this padding to remove 1px gap between list items
+            paddingRight: 1
+        },
+        itemLink: { fontSize: FontSizes.size14 }
+    };
+}
