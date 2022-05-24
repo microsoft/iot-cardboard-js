73cbb398b96899f0e4399be72ad97c326b1762b3
miltone92 <55605034+miltone92@users.noreply.github.com>
2022-05-24 17:34:29 -0600

    OAT-133 Merge changes

--- src/Pages/OATEditorPage/OATEditorPage.tsx
+++ src/Pages/OATEditorPage/OATEditorPage.tsx
@@ -3,7 +3,7 @@ import OATHeader from '../../Components/OATHeader/OATHeader';
 import OATModelList from '../../Components/OATModelList/OATModelList';
 import OATGraphViewer from '../../Components/OATGraphViewer/OATGraphViewer';
 import OATPropertyEditor from '../../Components/OATPropertyEditor/OATPropertyEditor';
-import { geteditorPageStyles } from './OATEditorPage.Styles';
+import { getEditorPageStyles } from './OATEditorPage.Styles';
 import { ErrorBoundary } from 'react-error-boundary';
 import {
     OATEditorPageReducer,
@@ -17,7 +17,7 @@ const OATEditorPage = ({ theme }) => {
         OATEditorPageReducer,
         defaultOATEditorState
     );
-    const editorPageStyles = geteditorPageStyles();
+    const editorPageStyles = getEditorPageStyles();
 
     const languages = Object.keys(i18n.options.resources).map((language) => {
         return {
