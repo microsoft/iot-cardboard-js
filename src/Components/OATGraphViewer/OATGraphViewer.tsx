87b5b78274dfeeed3d43406157a436d63e82327d
miltonzarmada <87083787+miltonzarmada@users.noreply.github.com>
2022-06-09 16:53:57 -0600

    ADTS-175

--- src/Components/OATGraphViewer/OATGraphViewer.tsx
+++ src/Components/OATGraphViewer/OATGraphViewer.tsx
@@ -24,7 +24,8 @@ import {
     OATRelationshipHandleName,
     OATExtendHandleName,
     OATInterfaceType,
-    OATComponentHandleName
+    OATComponentHandleName,
+    OATNamespaceDefaultValue
 } from '../../Models/Constants/Constants';
 import {
     getGraphViewerStyles,
@@ -51,7 +52,6 @@ import { ElementEdge } from './Internal/Classes/ElementEdge';
 import { ElementEdgeData } from './Internal/Classes/ElementEdgeData';
 import { deepCopy } from '../../Models/Services/Utils';
 
-const idClassBase = 'dtmi:com:example:';
 const contextClassBase = 'dtmi:dtdl:context;2';
 const versionClassBase = '1';
 const defaultNodePosition = 25;
@@ -241,7 +241,8 @@ const OATGraphViewer = ({ state, dispatch }: OATGraphProps) => {
         selectedModelId,
         editedModelName,
         editedModelId,
-        modelPositions
+        modelPositions,
+        namespace
     } = state;
     const { t } = useTranslation();
     const theme = useTheme();
@@ -265,6 +266,10 @@ const OATGraphViewer = ({ state, dispatch }: OATGraphProps) => {
 
     const dagreGraph = new dagre.graphlib.Graph();
     dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+    const idClassBase = `dtmi:${
+        namespace ? namespace : OATNamespaceDefaultValue
+    }:`;
 
     const applyLayoutToElements = (elements, direction = 'TB') => {
         const isHorizontal = direction === 'LR';
