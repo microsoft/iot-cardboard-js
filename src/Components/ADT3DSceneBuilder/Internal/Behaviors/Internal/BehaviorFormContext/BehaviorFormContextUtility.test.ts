323be5e95a9ad130d57aa896dddb9163079db7b8
Mitch Snyder <msnyder@microsoft.com>
2022-06-21 10:36:13 -0700

    Create test stub for utility

---
+++ src/Components/ADT3DSceneBuilder/Internal/Behaviors/Internal/BehaviorFormContext/BehaviorFormContextUtility.test.ts
@@ -0,0 +1,8 @@
+import { cleanup } from '@testing-library/react';
+
+describe('BehaviorFormContextUtility', () => {
+    afterEach(cleanup);
+    describe('Stuff', () => {
+        test('mytest', () => {});
+    });
+});
