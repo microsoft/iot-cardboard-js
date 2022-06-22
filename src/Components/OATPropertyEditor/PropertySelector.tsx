33abaa963b7d352fb001f614aba4f59e733992ef
JoseZarmada <jose@zarmada.com>
2022-06-22 14:44:21 -0700

    ADTS-173 Moving the data to constants

--- src/Components/OATPropertyEditor/PropertySelector.tsx
+++ src/Components/OATPropertyEditor/PropertySelector.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Separator, Stack } from '@fluentui/react';
+import { ActionButton, Separator, Stack } from '@fluentui/react';
 import {
     getPropertyInspectorStyles,
     getPropertySelectorSeparatorStyles
 } from './OATPropertyEditor.styles';
-import { DTDLSchemaType } from '../../Models/Classes/DTDL';
 import Svg from 'react-inlinesvg';
 import { useTranslation } from 'react-i18next';
 import { SET_OAT_PROPERTY_EDITOR_MODEL } from '../../Models/Constants/ActionTypes';
@@ -14,27 +13,9 @@ import {
 } from '../../Models/Constants/Interfaces';
 import { deepCopy } from '../../Models/Services/Utils';
 import { IOATEditorState } from '../../Pages/OATEditorPage/OATEditorPage.types';
-import IconBoolean from '../../Resources/Static/Boolean.svg';
-import IconData from '../../Resources/Static/Data.svg';
-import IconDatetime from '../../Resources/Static/Datetime.svg';
-import IconDouble from '../../Resources/Static/Double.svg';
-import IconDuration from '../../Resources/Static/duration.svg';
-import IconEnum from '../../Resources/Static/Enum.svg';
-import IconFloat from '../../Resources/Static/Float.svg';
-import IconInteger from '../../Resources/Static/Integer.svg';
-import IconLineString from '../../Resources/Static/linestring.svg';
-import IconLong from '../../Resources/Static/long.svg';
-import IconMap from '../../Resources/Static/map.svg';
-import IconMultiPoint from '../../Resources/Static/multipoint.svg';
-import IconMultiLineString from '../../Resources/Static/multilinestring.svg';
-import IconMultiPolygon from '../../Resources/Static/multipolygon.svg';
-import IconObject from '../../Resources/Static/object.svg';
-import IconPoint from '../../Resources/Static/point.svg';
-import IconPolygon from '../../Resources/Static/polygon.svg';
-import IconString from '../../Resources/Static/string.svg';
-import IconTime from '../../Resources/Static/time.svg';
 
 import { getModelPropertyCollectionName } from './Utils';
+import { propertySelectorData } from '../../Models/Constants';
 
 const versionClassBase = '1';
 const leftOffset = 170; // Place selector's most used options above trigger element
@@ -72,115 +53,6 @@ const PropertySelector = ({
     const propertiesKeyName = getModelPropertyCollectionName(
         model ? model['@type'] : null
     );
-
-    const data = {
-        propertyTags: {
-            sectionFirst: [
-                {
-                    name: 'boolean',
-                    title: t('OATPropertyEditor.boolean'),
-                    icon: IconBoolean
-                },
-                {
-                    name: 'float',
-                    title: t('OATPropertyEditor.float'),
-                    icon: IconFloat
-                },
-                {
-                    name: 'dateTime',
-                    title: t('OATPropertyEditor.dateTime'),
-                    icon: IconDatetime
-                },
-                {
-                    name: DTDLSchemaType.Enum,
-                    title: t('OATPropertyEditor.enum'),
-                    icon: IconEnum,
-                    complex: true
-                },
-                {
-                    name: 'double',
-                    title: t('OATPropertyEditor.double'),
-                    icon: IconDouble
-                },
-                {
-                    name: 'data',
-                    title: t('OATPropertyEditor.data'),
-                    icon: IconData
-                },
-                {
-                    name: 'integer',
-                    title: t('OATPropertyEditor.integer'),
-                    icon: IconInteger
-                },
-                {
-                    name: 'long',
-                    title: t('OATPropertyEditor.long'),
-                    icon: IconLong
-                },
-                {
-                    name: DTDLSchemaType.Map,
-                    title: t('OATPropertyEditor.map'),
-                    icon: IconMap,
-                    complex: true
-                },
-                {
-                    name: 'duration',
-                    title: t('OATPropertyEditor.duration'),
-                    icon: IconDuration
-                }
-            ],
-            sectionSecond: [
-                {
-                    name: 'string',
-                    title: t('OATPropertyEditor.string'),
-                    icon: IconString
-                },
-                {
-                    name: 'time',
-                    title: t('OATPropertyEditor.time'),
-                    icon: IconTime
-                },
-                {
-                    name: DTDLSchemaType.Object,
-                    title: t('OATPropertyEditor.object'),
-                    icon: IconObject,
-                    complex: true
-                }
-            ],
-            sectionThird: [
-                {
-                    name: 'point',
-                    title: t('OATPropertyEditor.point'),
-                    icon: IconPoint
-                },
-                {
-                    name: 'linestring',
-                    title: t('OATPropertyEditor.linestring'),
-                    icon: IconLineString
-                },
-                {
-                    name: 'polygon',
-                    title: t('OATPropertyEditor.polygon'),
-                    icon: IconPolygon
-                },
-                {
-                    name: 'multiPoint',
-                    title: t('OATPropertyEditor.multiPoint'),
-                    icon: IconMultiPoint
-                },
-                {
-                    name: 'multiLinestring',
-                    title: t('OATPropertyEditor.multiLinestring'),
-                    icon: IconMultiLineString
-                },
-                {
-                    name: 'multiPolygon',
-                    title: t('OATPropertyEditor.multiPolygon'),
-                    icon: IconMultiPolygon
-                }
-            ]
-        }
-    };
 
     const addNestedProperty = (tag: string) => {
         const modelCopy = deepCopy(model);
@@ -291,83 +163,100 @@ const PropertySelector = ({
         >
             <Stack horizontal>
                 <div className={propertyInspectorStyles.propertyTagsWrapSecond}>
-                    {data.propertyTags.sectionSecond.map((tag, i) => {
-                        if (
-                            lastPropertyFocused &&
-                            typeof lastPropertyFocused.item.schema ===
-                                'object' &&
-                            tag.complex
-                        ) {
-                            return <></>;
-                        } else {
-                            return (
-                                <Svg
-                                    tabIndex={0}
-                                    key={i}
-                                    className={
-                                        propertyInspectorStyles.propertyTag
-                                    }
-                                    onClick={() => {
-                                        handleTagClick(tag.name);
-                                    }}
-                                    onKeyPress={() => {
-                                        handleTagClick(tag.name);
-                                    }}
-                                    src={tag.icon}
-                                    title={tag.title}
-                                ></Svg>
-                            );
+                    {propertySelectorData.propertyTags.sectionSecond.map(
+                        (tag, i) => {
+                            if (
+                                lastPropertyFocused &&
+                                typeof lastPropertyFocused.item.schema ===
+                                    'object' &&
+                                tag.complex
+                            ) {
+                                return <></>;
+                            } else {
+                                return (
+                                    <ActionButton
+                                        onClick={() => {
+                                            handleTagClick(tag.name);
+                                        }}
+                                        onKeyPress={() => {
+                                            handleTagClick(tag.name);
+                                        }}
+                                    >
+                                        <Svg
+                                            tabIndex={0}
+                                            key={i}
+                                            className={
+                                                propertyInspectorStyles.propertyTag
+                                            }
+                                            src={tag.icon}
+                                            title={t(tag.title)}
+                                        ></Svg>
+                                    </ActionButton>
+                                );
+                            }
                         }
-                    })}
+                    )}
                 </div>
                 <Separator styles={propertySelectorSeparatorStyles} vertical />
                 <div className={propertyInspectorStyles.propertyTagsWrapFirst}>
-                    {data.propertyTags.sectionFirst.map((tag, i) => {
-                        if (
-                            lastPropertyFocused &&
-                            typeof lastPropertyFocused.item.schema ===
-                                'object' &&
-                            tag.complex
-                        ) {
-                            return <></>;
-                        } else {
-                            return (
+                    {propertySelectorData.propertyTags.sectionFirst.map(
+                        (tag, i) => {
+                            if (
+                                lastPropertyFocused &&
+                                typeof lastPropertyFocused.item.schema ===
+                                    'object' &&
+                                tag.complex
+                            ) {
+                                return <></>;
+                            } else {
+                                return (
+                                    <ActionButton
+                                        onClick={() => {
+                                            handleTagClick(tag.name);
+                                        }}
+                                        onKeyPress={() => {
+                                            handleTagClick(tag.name);
+                                        }}
+                                    >
+                                        <Svg
+                                            tabIndex={0}
+                                            key={i}
+                                            className={
+                                                propertyInspectorStyles.propertyTag
+                                            }
+                                            src={tag.icon}
+                                            title={t(tag.title)}
+                                        ></Svg>
+                                    </ActionButton>
+                                );
+                            }
+                        }
+                    )}
+                </div>
+                <Separator styles={propertySelectorSeparatorStyles} vertical />
+                <div className={propertyInspectorStyles.propertyTagsWrapThird}>
+                    {propertySelectorData.propertyTags.sectionThird.map(
+                        (tag, i) => (
+                            <ActionButton
+                                onClick={() => {
+                                    handleTagClick(tag.name);
+                                }}
+                                onKeyPress={() => {
+                                    handleTagClick(tag.name);
+                                }}
+                            >
                                 <Svg
                                     tabIndex={0}
                                     key={i}
                                     className={
                                         propertyInspectorStyles.propertyTag
                                     }
-                                    onClick={() => {
-                                        handleTagClick(tag.name);
-                                    }}
-                                    onKeyPress={() => {
-                                        handleTagClick(tag.name);
-                                    }}
                                     src={tag.icon}
-                                    title={tag.title}
+                                    title={t(tag.title)}
                                 ></Svg>
-                            );
-                        }
-                    })}
-                </div>
-                <Separator styles={propertySelectorSeparatorStyles} vertical />
-                <div className={propertyInspectorStyles.propertyTagsWrapThird}>
-                    {data.propertyTags.sectionThird.map((tag, i) => (
-                        <Svg
-                            tabIndex={0}
-                            key={i}
-                            className={propertyInspectorStyles.propertyTag}
-                            onClick={() => {
-                                handleTagClick(tag.name);
-                            }}
-                            onKeyPress={() => {
-                                handleTagClick(tag.name);
-                            }}
-                            src={tag.icon}
-                            title={tag.title}
-                        ></Svg>
-                    ))}
+                            </ActionButton>
+                        )
+                    )}
                 </div>
             </Stack>
         </div>
