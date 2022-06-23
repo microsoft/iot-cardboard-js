63435d03265981109eb69e3ac647d388b44b6d24
Cory Crowley <ccrowley96@gmail.com>
2022-06-23 14:52:12 -0700

    feat: Logging and Telemetry Framework (#583)
    * Set up telemetry service
    
    * added basic logging service
    
    * Design review updates
    
    * Added tests for TelemetryService
    
    * Update this context binding in logging service
    
    * changed telemetry service import to full file path in test (speeds up test significantly when not importing from index file)
    
    * Renamed logging hook to useLogger

--- src/Models/Services/index.ts
+++ src/Models/Services/index.ts
@@ -1,2 +1,5 @@
 export { default as MsalAuthService } from './MsalAuthService';
+export { default as TelemetryService } from './TelemetryService/TelemetryService';
+export * as Telemetry from './TelemetryService/Telemetry';
+export * as TelemetryTypes from './TelemetryService/TelemetryService.types';
 export * as Utils from './Utils';
