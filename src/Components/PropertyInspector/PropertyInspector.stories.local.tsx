78e9626b066ea87112bf7d021bdbc7b1a2adcad7
Brenna Courtney <t-brcourtney@microsoft.com>
2022-07-22 10:51:04 -0700

    fixed error where PI could not read fields of properties that were enums

--- src/Components/PropertyInspector/PropertyInspector.stories.local.tsx
+++ src/Components/PropertyInspector/PropertyInspector.stories.local.tsx
@@ -43,7 +43,7 @@ export const AdtTwin = (args, { globals: { theme, locale } }) => {
 AdtTwin.argTypes = {
     twinId: {
         control: { type: 'text' },
-        defaultValue: 'PasteurizationMachine_A01'
+        defaultValue: 'ActivePowerSensorTwin'
     }
 };
 
