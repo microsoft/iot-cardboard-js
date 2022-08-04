7d24dbf42caf0712a9c739806ee4f1ed1873d41d
Brenna Courtney <t-brcourtney@microsoft.com>
2022-08-04 11:35:45 -0700

    move null check

--- src/Components/PropertyInspector/PropertyInspectoryModel.ts
+++ src/Components/PropertyInspector/PropertyInspectoryModel.ts
@@ -656,8 +656,8 @@ abstract class PropertyInspectorModel {
 
     static conformSyntax = (inputEl) => {
         // Update syntax of all object keys
-        if (!inputEl) return;
         if (typeof inputEl === 'object') {
+            if (!inputEl) return;
             Object.keys(inputEl).forEach((key) => {
                 // Update to simplified DTDL syntax (conform to our interface)
                 if (key in dtdlSyntaxMap) {
