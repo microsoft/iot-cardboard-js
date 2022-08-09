43269260beb5f1c7b6c22984df22e4a80a5e096a
Brenna Courtney <t-brcourtney@microsoft.com>
2022-08-09 14:34:30 -0700

    providing context

--- src/Components/ADT3DBuilder/ADT3DBuilder.stories.local.tsx
+++ src/Components/ADT3DBuilder/ADT3DBuilder.stories.local.tsx
@@ -3,6 +3,7 @@ import MsalAuthService from '../../Models/Services/MsalAuthService';
 import ADT3DBuilder from './ADT3DBuilder';
 import useAuthParams from '../../../.storybook/useAuthParams';
 import ADTAdapter from '../../Adapters/ADTAdapter';
+import { SceneThemeContextProvider } from '../../Models/Context';
 
 export default {
     title: '3DV/ADT3DBuilder',
@@ -20,19 +21,21 @@ export const Engine = () => {
         <div></div>
     ) : (
         <div style={{ width: '600px', height: '400px' }}>
-            <ADT3DBuilder
-                title="3D Builder"
-                adapter={
-                    new ADTAdapter(
-                        authenticationParameters.adt.hostUrl,
-                        new MsalAuthService(
-                            authenticationParameters.adt.aadParameters
+            <SceneThemeContextProvider>
+                <ADT3DBuilder
+                    title="3D Builder"
+                    adapter={
+                        new ADTAdapter(
+                            authenticationParameters.adt.hostUrl,
+                            new MsalAuthService(
+                                authenticationParameters.adt.aadParameters
+                            )
                         )
-                    )
-                }
-                modelUrl="https://cardboardresources.blob.core.windows.net/3dv-workspace-2/TruckBoxesEnginesPastmachine.gltf"
-                onMeshClicked={onMeshClicked}
-            />
+                    }
+                    modelUrl="https://cardboardresources.blob.core.windows.net/3dv-workspace-2/TruckBoxesEnginesPastmachine.gltf"
+                    onMeshClicked={onMeshClicked}
+                />
+            </SceneThemeContextProvider>
         </div>
     );
 };
