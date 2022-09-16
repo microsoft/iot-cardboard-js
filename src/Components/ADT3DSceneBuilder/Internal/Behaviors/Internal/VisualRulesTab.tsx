6aacc689f6485312b480a69e29bed27e78c842b7
Patricio Sanchez <pasanch@microsoft.com>
2022-09-16 10:00:08 -0700

    Fix prop

--- src/Components/ADT3DSceneBuilder/Internal/Behaviors/Internal/VisualRulesTab.tsx
+++ src/Components/ADT3DSceneBuilder/Internal/Behaviors/Internal/VisualRulesTab.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { VisualRulesList } from '../VisualRules/VisualRuleList';
 
 export const VisualRulesTab: React.FC = () => {
-    return <VisualRulesList ruleList={[]} />;
+    return <VisualRulesList ruleItems={[]} />;
 };
