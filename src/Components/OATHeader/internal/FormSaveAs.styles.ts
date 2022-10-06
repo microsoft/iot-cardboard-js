0bdc515d3a4f9e1b50975903c8b3f2d6569fde14
Mitch Snyder <msnyder@microsoft.com>
2022-10-06 12:59:28 -0700

    Delete old command bar files

--- src/Components/OATHeader/internal/FormSaveAs.styles.ts
+++
@@ -1,6 +0,0 @@
-import { IFormOpenStyleProps, IFormOpenStyles } from './FormOpen.types';
-import { getCommonModalStyles } from './Shared.styles';
-
-export const getStyles = (props: IFormOpenStyleProps): IFormOpenStyles => {
-    return getCommonModalStyles(props);
-};
