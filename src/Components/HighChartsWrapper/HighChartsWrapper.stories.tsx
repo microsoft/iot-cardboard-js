da3eda56a5f9df4aa996178370c02f90189abc32
Irem Gokce Aydin <45217314+iremgokce@users.noreply.github.com>
2022-10-27 13:09:00 -0700

    fix: [Data history]: Fixed data grouping for large data set (#742)
    * fixed import for data grouping to work
    
    * moved mock data to mock file
    
    * lint fix

--- src/Components/HighChartsWrapper/HighChartsWrapper.stories.tsx
+++ src/Components/HighChartsWrapper/HighChartsWrapper.stories.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { ComponentStory } from '@storybook/react';
 import { getDefaultStoryDecorator } from '../../Models/Services/StoryUtilities';
 import HighChartsWrapper from './HighChartsWrapper';
+import { IHighChartsWrapperProps } from './HighChartsWrapper.types';
 import {
     HighChartsMockData,
-    IHighChartsWrapperProps
-} from './HighChartsWrapper.types';
+    HighChartsMockLargeData
+} from './HighChartsWrapper.mock';
 
 const wrapperStyle = { width: '500px', height: '300px', padding: 8 };
 
@@ -55,4 +56,10 @@ FixedTimeRange.args = {
         xMinInMillis: Date.UTC(2012, 5, 16).valueOf(),
         xMaxInMillis: Date.UTC(2012, 5, 26).valueOf()
     }
+} as IHighChartsWrapperProps;
+
+export const LargeDataSet = Template.bind({}) as HighChartsWrapperStory;
+LargeDataSet.args = {
+    ...commonArgs,
+    seriesData: HighChartsMockLargeData
 } as IHighChartsWrapperProps;
--- src/Components/HighChartsWrapper/HighChartsWrapper.types.ts
+++ src/Components/HighChartsWrapper/HighChartsWrapper.types.ts
@@ -73,39 +73,3 @@ export interface IHighChartsWrapperSubComponentStyles {
     loadingText?: { root: CSSObject };
     noDataText?: { root: CSSObject };
 }
-
-export const HighChartsMockData: Array<IHighChartSeriesData> = [
-    {
-        name: 'Mock series-1',
-        data: [
-            { timestamp: 1340323200000, value: 115 },
-            { timestamp: 1340339499317, value: 23 },
-            { timestamp: 1340368246728, value: 188 },
-            { timestamp: 1340390085747, value: 213 },
-            { timestamp: 1340403271088, value: 245 }
-        ],
-        tooltipSuffix: '°F'
-    },
-    {
-        name: 'Mock series-2',
-        data: [
-            { timestamp: 1340329513434, value: 272 },
-            { timestamp: 1340339253976, value: 169 },
-            { timestamp: 1340341675699, value: 238 },
-            { timestamp: 1340385501865, value: 395 },
-            { timestamp: 1340390604011, value: 109 }
-        ],
-        tooltipSuffix: 'cpf'
-    },
-    {
-        name: 'Mock series-3',
-        data: [
-            { timestamp: 1340323200000, value: 279 },
-            { timestamp: 1340332010046, value: 154 },
-            { timestamp: 1340356361575, value: 157 },
-            { timestamp: 1340395911660, value: 227 },
-            { timestamp: 1340401653986, value: 107 }
-        ],
-        tooltipSuffix: 'm'
-    }
-];
