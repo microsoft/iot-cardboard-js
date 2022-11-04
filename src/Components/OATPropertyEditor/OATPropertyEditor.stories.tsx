f28243ea8ad3f63c1dbe055458b43960dfb1d423
Mitch Snyder <msnyder@microsoft.com>
2022-11-04 14:20:31 -0700

    Story for JSON

--- src/Components/OATPropertyEditor/OATPropertyEditor.stories.tsx
+++ src/Components/OATPropertyEditor/OATPropertyEditor.stories.tsx
@@ -18,11 +18,13 @@ import { ComponentStory } from '@storybook/react';
 import { IOatPageContextState } from '../../Models/Context/OatPageContext/OatPageContext.types';
 import {
     IStoryContext,
-    getDefaultStoryDecorator
+    getDefaultStoryDecorator,
+    sleep
 } from '../../Models/Services/StoryUtilities';
 import { getTargetFromSelection } from './Utils';
 import { IOATSelection } from '../../Pages/OATEditorPage/OATEditorPage.types';
 import { IOATFile } from '../../Pages/OATEditorPage/Internal/Classes/OatTypes';
+import { userEvent, within } from '@storybook/testing-library';
 
 const wrapperStyle: React.CSSProperties = {
     width: 'auto',
@@ -119,8 +121,8 @@ const getMockModel = () => {
     return model;
 };
 
-export const ModelSelected = Template.bind({});
-ModelSelected.args = (() => {
+export const ModelSelectedEditor = Template.bind({});
+ModelSelectedEditor.args = (() => {
     const files = getMockFiles();
     const args: StoryProps = {
         files: files,
@@ -128,3 +130,23 @@ ModelSelected.args = (() => {
     };
     return args;
 })();
+
+export const ModelSelectedJson = Template.bind({});
+ModelSelectedJson.args = (() => {
+    const files = getMockFiles();
+    const args: StoryProps = {
+        files: files,
+        selection: { modelId: files[0].data.models[0]['@id'] }
+    };
+    return args;
+})();
+ModelSelectedJson.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    // Finds the menu and opens it
+    const tab = (await canvas.findAllByRole('tab'))[1];
+    userEvent.click(tab);
+
+    // wait for the menu
+    await sleep(10);
+};
