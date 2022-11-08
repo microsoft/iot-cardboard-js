1a3b300a8c2c63a9a96f05f2d8033f36f925ffdd
Alex Hayward <alex.hayward@gmail.com>
2022-11-08 09:59:33 +0000

    added string to list of allowed property schemas in timeseries builder

--- src/Components/ADT3DSceneBuilder/Internal/Behaviors/Widgets/WidgetBuilders/DataHistoryWidgetBuilder/Internal/TimeSeriesFormCallout.tsx
+++ src/Components/ADT3DSceneBuilder/Internal/Behaviors/Widgets/WidgetBuilders/DataHistoryWidgetBuilder/Internal/TimeSeriesFormCallout.tsx
@@ -31,6 +31,7 @@ import {
     PropertyExpression
 } from '../../../../../../../ModelledPropertyBuilder/ModelledPropertyBuilder.types';
 import { createGUID } from '../../../../../../../../Models/Services/Utils';
+import { PropertyValueType } from '../../../../../../../../Models/Constants/Constants';
 
 interface IProp {
     calloutTarget: string;
@@ -48,6 +49,12 @@ const defaultSeries: IDataHistoryBasicTimeSeries = {
     id: '',
     expression: ''
 };
+
+// Allow numerics + string as users often use string properties as the lowest common denominator
+const allowedTimeseriesPropertyValueTypes: Array<PropertyValueType> = [
+    'string',
+    ...numericPropertyValueTypes
+];
 
 /** This callout component consists form for time series authoring to show in line chart */
 const TimeSeriesFormCallout: React.FC<IProp> = ({
@@ -152,7 +159,9 @@ const TimeSeriesFormCallout: React.FC<IProp> = ({
                         expression: seriesToEdit?.expression
                     }}
                     onChange={handlePropertyChange}
-                    allowedPropertyValueTypes={numericPropertyValueTypes}
+                    allowedPropertyValueTypes={
+                        allowedTimeseriesPropertyValueTypes
+                    }
                     required
                 />
                 <TextField
