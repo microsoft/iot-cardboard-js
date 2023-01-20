2ff324a3865f067bca00d117d2a4d26aeb715f39
Irem Gokce Aydin <iremay@microsoft.com>
2023-01-20 14:30:15 -0800

    test fix

--- src/Models/Services/__tests__/DataHistoryUtils.test.ts
+++ src/Models/Services/__tests__/DataHistoryUtils.test.ts
@@ -8,6 +8,7 @@ import {
     getQuickTimeSpanKeyByValue,
     transformADXTimeSeriesToHighChartsSeries
 } from '../../SharedUtils/DataHistoryUtils';
+import { createGUID } from '../Utils';
 
 afterEach(cleanup);
 
@@ -41,8 +42,10 @@ describe('DataHistoryUtils', () => {
     describe('transformADXTimeSeriesToHighChartsSeries', () => {
         test('merge ADX data with authored time series object for chart to render', () => {
             // ARRANGE
+            const seriesId = createGUID();
             const adxData: Array<ADXTimeSeries> = [
                 {
+                    seriesId,
                     id: 'SaltMachine_C0',
                     key: 'InFlow',
                     data: [
@@ -56,7 +59,7 @@ describe('DataHistoryUtils', () => {
             ];
             const timeSeries: Array<IDataHistoryTimeSeriesTwin> = [
                 {
-                    seriesId: '',
+                    seriesId,
                     twinId: 'SaltMachine_C0',
                     twinPropertyName: 'InFlow',
                     twinPropertyType: 'double',
