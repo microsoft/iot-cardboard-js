8d7e7b0ae3d010f3536342417da1b25ff8dcc8f4
ptallettms <85894309+ptallettms@users.noreply.github.com>
2023-02-01 18:31:03 +0000

    Revert export

--- src/Models/Hooks/index.ts
+++ src/Models/Hooks/index.ts
@@ -4,4 +4,3 @@ export { default as useGuid } from './useGuid';
 export { default as useLongPoll } from './useLongPoll';
 export { default as useTSIChartComponentRender } from './useRenderTSIChartComponent';
 export { default as useRuntimeSceneData } from './useRuntimeSceneData';
-export { I3DScenesConfig } from '../Types/Generated/3DScenesConfiguration-v1.0.0';
