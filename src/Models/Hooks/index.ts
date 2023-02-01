caccce2ded4f8884ff2f22641543af110033aeca
ptallettms <85894309+ptallettms@users.noreply.github.com>
2023-02-01 18:18:53 +0000

    Bug fix for runtime hook

--- src/Models/Hooks/index.ts
+++ src/Models/Hooks/index.ts
@@ -4,3 +4,4 @@ export { default as useGuid } from './useGuid';
 export { default as useLongPoll } from './useLongPoll';
 export { default as useTSIChartComponentRender } from './useRenderTSIChartComponent';
 export { default as useRuntimeSceneData } from './useRuntimeSceneData';
+export { I3DScenesConfig } from '../Types/Generated/3DScenesConfiguration-v1.0.0';
