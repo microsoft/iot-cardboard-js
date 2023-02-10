50da8a7f76c9755a0843f956afdfefe71e6137b3
Mitch Snyder <msnyder@microsoft.com>
2023-02-10 11:15:43 -0800

    Fix story

--- src/Components/OATPropertyEditor/Internal/Version3UpgradeButton/Version3UpgradeButton.stories.tsx
+++ src/Components/OATPropertyEditor/Internal/Version3UpgradeButton/Version3UpgradeButton.stories.tsx
@@ -3,6 +3,9 @@ import { ComponentStory } from '@storybook/react';
 import { getDefaultStoryDecorator } from '../../../../Models/Services/StoryUtilities';
 import Version3UpgradeButton from './Version3UpgradeButton';
 import { IVersion3UpgradeButtonProps } from './Version3UpgradeButton.types';
+import { OatPageContextProvider } from '../../../../Models/Context/OatPageContext/OatPageContext';
+import { getMockModelItem } from '../../../../Models/Context/OatPageContext/OatPageContext.mock';
+import { DTDL_CONTEXT_VERSION_2 } from '../../../../Models/Classes/DTDL';
 
 const wrapperStyle = { width: '100%', height: '600px', padding: 8 };
 
@@ -17,8 +20,14 @@ export default {
 type Version3UpgradeButtonStory = ComponentStory<typeof Version3UpgradeButton>;
 
 const Template: Version3UpgradeButtonStory = (args) => {
-    return <Version3UpgradeButton {...args} />;
+    return (
+        <OatPageContextProvider disableLocalStorage={true}>
+            <Version3UpgradeButton {...args} />
+        </OatPageContextProvider>
+    );
 };
 
 export const Base = Template.bind({}) as Version3UpgradeButtonStory;
-Base.args = {} as IVersion3UpgradeButtonProps;
+Base.args = {
+    selectedModel: getMockModelItem('dtmi:model1;1', DTDL_CONTEXT_VERSION_2)
+} as IVersion3UpgradeButtonProps;
