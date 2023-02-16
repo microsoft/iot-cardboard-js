4b2e46081b774c643d05ffb3797550e42a499587
Mitch Snyder <msnyder@microsoft.com>
2023-02-15 16:49:28 -0800

    Fix story path

--- src/Components/OATPropertyEditor/Internal/JSONEditor/JSONEditor.stories.tsx
+++ src/Components/OATPropertyEditor/Internal/JSONEditor/JSONEditor.stories.tsx
@@ -9,7 +9,7 @@ import { CommandHistoryContextProvider } from '../../../../Pages/OATEditorPage/I
 const wrapperStyle = { width: '100%', height: '600px', padding: 8 };
 
 export default {
-    title: 'Components/JSONEditor',
+    title: 'Components - OAT/OATPropertyEditor/JSONEditor',
     component: JSONEditor,
     decorators: [getDefaultStoryDecorator<IJSONEditorProps>(wrapperStyle)]
 };
