0ce76121cfbdde533e42fc4a8dd440e3f93e9452
Irem Gokce Aydin <iremay@microsoft.com>
2023-02-16 11:48:40 -0800

    removed unused

--- src/Components/HighChartsWrapper/HighChartsWrapper.mock.ts
+++ src/Components/HighChartsWrapper/HighChartsWrapper.mock.ts
@@ -40,12 +40,7 @@ export const HighChartsMockData: Array<IHighChartSeriesData> = [
 export const HighChartsMockLargeData: Array<IHighChartSeriesData> = [
     {
         name: 'Mock series-1',
-        data: getMockTimeSeriesDataArrayInLocalTime(
-            1,
-            500000,
-            undefined,
-            true
-        )[0],
+        data: getMockTimeSeriesDataArrayInLocalTime(1, 500000)[0],
         tooltipSuffix: '°F'
     }
 ];
