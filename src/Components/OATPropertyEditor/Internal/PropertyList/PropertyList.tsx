8b1f2516141090b4c077504aaeba7bbf639e6e36
Mitch Snyder <msnyder@microsoft.com>
2023-02-21 16:26:20 -0800

    REvert

--- src/Components/OATPropertyEditor/Internal/PropertyList/PropertyList.tsx
+++ src/Components/OATPropertyEditor/Internal/PropertyList/PropertyList.tsx
@@ -175,7 +175,7 @@ const PropertyList: React.FC<IPropertyListProps> = (props) => {
                     }
                 });
             };
-            if (isDTDLModel(selectedI tem)) {
+            if (isDTDLModel(selectedItem)) {
                 // update for model
                 const updatedContents = [...selectedItem.contents];
                 const index = getPropertyIndexOnModelByName(
