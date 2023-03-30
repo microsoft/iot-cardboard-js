79bb625913d833870efb42fd9caad000150424af
Irem Gokce Aydin <iremay@microsoft.com>
2023-03-30 11:43:13 -0700

    added types for cooking

--- src/Apps/Legion/Models/Interfaces.ts
+++ src/Apps/Legion/Models/Interfaces.ts
@@ -1,2 +1,33 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface IBaseAdapter {}
+
+export interface IModel {
+    id: string;
+    name: string;
+    propertyIds: Array<string>; // FKs for IModelProperty
+}
+
+export interface IModelProperty {
+    id: string;
+    name: string;
+    sourcePropName: string;
+}
+
+export interface ITwin {
+    id: string;
+    name: string;
+    modelId: string; // FK for IModel
+    sourceConnectionString: string;
+}
+
+export interface IRelationshipModel {
+    id: string;
+    name: string;
+}
+
+export interface IRelationship {
+    id: string;
+    relationshipModelId: string; // FK for IRelationshipModel
+    sourceTwinId: string; // FK for ITwin
+    targetTwinId: string; // FK for ITwin
+}
