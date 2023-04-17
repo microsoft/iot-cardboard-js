a5b16b5dc6a45a3fc02345af17b6e9c72a37e2a8
Mitch Snyder <msnyder@microsoft.com>
2023-04-17 11:22:22 -0700

    Legion: Add hooks & types for accessing new data model (#927)
    * Add new types
    * Add new hooks 
    * Add new context for wizard data

---
+++ src/Apps/Legion/Contexts/WizardDataContext/WizardDataContext.types.ts
@@ -0,0 +1,136 @@
+import {
+    IDbEntity,
+    IDbProperty,
+    IDbRelationship,
+    IDbRelationshipType,
+    IDbType
+} from '../../Models';
+
+export interface IWizardDataContextProviderProps<N> {
+    initialState?: Partial<IWizardDataContextState>;
+}
+
+/**
+ * A context used for accessing the current wizard state
+ */
+export interface IWizardDataStateContext {
+    wizardDataState: IWizardDataContextState;
+}
+
+/**
+ * A context used for updating the wizard data state
+ */
+export interface IWizardDataDispatchContext {
+    wizardDataDispatch: React.Dispatch<WizardDataContextAction>;
+}
+
+/**
+ * The state of the context
+ */
+export interface IWizardDataContextState {
+    /** all of the `Entities` in the graph */
+    entities: IDbEntity[];
+    /** all of `Types` in the graph */
+    types: IDbType[];
+    /** all `Relationships` in the graph */
+    relationships: IDbRelationship[];
+    /** all `RelationshipTypes` in the graph */
+    relationshipTypes: IDbRelationshipType[];
+    /** all `Property` items in the graph */
+    properties: IDbProperty[];
+}
+
+/**
+ * The actions to update the state
+ */
+export enum WizardDataContextActionType {
+    ENTITY_ADD = 'ENTITY_ADD',
+    ENTITY_REMOVE = 'ENTITY_REMOVE',
+    ENTITY_UPDATE = 'ENTITY_UPDATE',
+    TYPE_ADD = 'TYPE_ADD',
+    TYPE_REMOVE = 'TYPE_REMOVE',
+    TYPE_UPDATE = 'TYPE_UPDATE',
+    RELATIONSHIP_ADD = 'RELATIONSHIP_ADD',
+    RELATIONSHIP_REMOVE = 'RELATIONSHIP_REMOVE',
+    RELATIONSHIP_UPDATE = 'RELATIONSHIP_UPDATE',
+    PROPERTY_ADD = 'PROPERTY_ADD',
+    PROPERTY_REMOVE = 'PROPERTY_REMOVE',
+    PROPERTY_UPDATE = 'PROPERTY_UPDATE'
+}
+
+/** The actions to update the state */
+export type WizardDataContextAction =
+    | {
+          type: WizardDataContextActionType.ENTITY_ADD;
+          payload: {
+              entity: IDbEntity;
+          };
+      }
+    | {
+          type: WizardDataContextActionType.ENTITY_REMOVE;
+          payload: {
+              entityId: string;
+          };
+      }
+    | {
+          type: WizardDataContextActionType.ENTITY_UPDATE;
+          payload: {
+              entity: IDbEntity;
+          };
+      }
+    | {
+          type: WizardDataContextActionType.TYPE_ADD;
+          payload: {
+              type: IDbType;
+          };
+      }
+    | {
+          type: WizardDataContextActionType.TYPE_REMOVE;
+          payload: {
+              typeId: string;
+          };
+      }
+    | {
+          type: WizardDataContextActionType.TYPE_UPDATE;
+          payload: {
+              type: IDbType;
+          };
+      }
+    | {
+          type: WizardDataContextActionType.RELATIONSHIP_ADD;
+          payload: {
+              relationship: IDbRelationship;
+              relationshipType: IDbRelationshipType;
+          };
+      }
+    | {
+          type: WizardDataContextActionType.RELATIONSHIP_REMOVE;
+          payload: {
+              relationshipId: string;
+          };
+      }
+    | {
+          type: WizardDataContextActionType.RELATIONSHIP_UPDATE;
+          payload: {
+              relationship: IDbRelationship;
+              relationshipType: IDbRelationshipType;
+          };
+      }
+    | {
+          type: WizardDataContextActionType.PROPERTY_ADD;
+          payload: {
+              property: IDbProperty;
+          };
+      }
+    | {
+          type: WizardDataContextActionType.PROPERTY_REMOVE;
+          payload: {
+              propertyId: string;
+          };
+      }
+    | {
+          type: WizardDataContextActionType.PROPERTY_UPDATE;
+          payload: {
+              property: IDbProperty;
+          };
+      };
---
+++ src/Apps/Legion/Hooks/useTypes.tsx
@@ -0,0 +1,97 @@
+import { useCallback, useMemo } from 'react';
+import {
+    useWizardDataDispatchContext,
+    useWizardDataStateContext
+} from '../Contexts/WizardDataContext/WizardDataContext';
+import { WizardDataContextActionType } from '../Contexts/WizardDataContext/WizardDataContext.types';
+import { IViewType } from '../Models';
+import {
+    convertTypeToDb,
+    convertTypeToView
+} from '../Services/AppTypeUtilities';
+import { getDebugLogger } from '../../../Models/Services/Utils';
+
+const debugLogging = false;
+export const logDebugConsole = getDebugLogger('useTypes', debugLogging);
+export const useTypes = () => {
+    // contexts
+    const { wizardDataDispatch } = useWizardDataDispatchContext();
+    const { wizardDataState } = useWizardDataStateContext();
+
+    // callbacks
+    const addType = useCallback(
+        (type: IViewType) => {
+            const newType = convertTypeToDb(type);
+            logDebugConsole('info', 'Adding Type to state. {Type}', type);
+            wizardDataDispatch({
+                type: WizardDataContextActionType.TYPE_ADD,
+                payload: {
+                    type: newType
+                }
+            });
+        },
+        [wizardDataDispatch]
+    );
+    const updateType = useCallback(
+        (updatedType: IViewType) => {
+            const type = convertTypeToDb(updatedType);
+            logDebugConsole(
+                'info',
+                `Updating Type (id: ${updatedType.id}) in state. {Type}`,
+                type
+            );
+            wizardDataDispatch({
+                type: WizardDataContextActionType.TYPE_UPDATE,
+                payload: {
+                    type: type
+                }
+            });
+        },
+        [wizardDataDispatch]
+    );
+    const deleteType = useCallback(
+        (typeId: string) => {
+            logDebugConsole(
+                'info',
+                `Removing Type (id: ${typeId}) from state. {id}`,
+                typeId
+            );
+            wizardDataDispatch({
+                type: WizardDataContextActionType.TYPE_REMOVE,
+                payload: {
+                    typeId: typeId
+                }
+            });
+        },
+        [wizardDataDispatch]
+    );
+
+    // data
+    const types: IViewType[] = useMemo(
+        () =>
+            wizardDataState.types.map((x) =>
+                convertTypeToView(x, wizardDataState)
+            ),
+        [wizardDataState]
+    );
+
+    return {
+        /** the current list of types in the state */
+        types: types,
+        /**
+         * Callback to add an type to the state
+         * NOTE: this is not a deep add. It will only add the root level element
+         */
+        addProperty: addType,
+        /**
+         * Callback to update the attributes of the type.
+         * NOTE: this is not a deep update. It will only reflect changes on the root level
+         */
+        updateProperty: updateType,
+        /**
+         * Callback to delete the type from state.
+         * NOTE: this is not a deep update. It will only delete the root level element
+         */
+        deleteProperty: deleteType
+    };
+};
