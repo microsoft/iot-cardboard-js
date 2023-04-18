9ef2184c01362fdee2d3078bacf14c4a594a16e0
Mitch Snyder <msnyder@microsoft.com>
2023-04-18 11:05:03 -0700

    Create mock data

--- src/Apps/Legion/Components/WizardShell/WizardShellMockData.ts
+++ src/Apps/Legion/Components/WizardShell/WizardShellMockData.ts
@@ -274,35 +274,260 @@ const getMockRelationshipType = (
     };
 };
 
-export const GET_DEFAULT_MOCK_WIZARD_DATA_CONTEXT = (): IWizardDataContextState => {
-    const prop1 = getProperty({ friendlyName: 'property1' });
-    const type1 = getType({ friendlyName: '', propertyIds: [prop1.id] });
-    const entity1 = getEntity({
-        friendlyName: 'Some entity 1',
-        typeId: type1.id
+const setDairyData = (data: IWizardDataContextState) => {
+    // PROPERTIES
+    const propTemp = getProperty({
+        friendlyName: 'Temperature',
+        sourcePropId: 'Temp'
     });
-    const entity2 = getEntity({
-        friendlyName: 'Some entity 2',
-        typeId: type1.id
+    const propPress = getProperty({
+        friendlyName: 'Pressure',
+        sourcePropId: 'Press'
     });
+    const propHum = getProperty({
+        friendlyName: 'Humidity',
+        sourcePropId: 'Hum'
+    });
+    const propFan = getProperty({
+        friendlyName: 'Fan Speed',
+        sourcePropId: 'FanSpd'
+    });
+    const propInflow = getProperty({
+        friendlyName: 'Inflow',
+        sourcePropId: 'InFlow'
+    });
+    const propOutflow = getProperty({
+        friendlyName: 'Outflow',
+        sourcePropId: 'OutFlow'
+    });
+    const propXPos = getProperty({
+        friendlyName: 'xPos',
+        sourcePropId: 'xPos'
+    });
+    const propYPos = getProperty({
+        friendlyName: 'yPos',
+        sourcePropId: 'yPos'
+    });
+    data.properties = [
+        propTemp,
+        propPress,
+        propHum,
+        propFan,
+        propInflow,
+        propOutflow,
+        propXPos,
+        propYPos
+    ];
 
-    const relationshipType1 = getMockRelationshipType({
-        name: 'Parent'
+    // TYPES
+    const typePastTs = getType({
+        friendlyName: 'Pasteurizer TS',
+        color: 'C13099',
+        icon: 'LineChart',
+        kind: Kind.TimeSeries,
+        propertyIds: [propTemp.id, propPress.id]
     });
-    const relationship1 = getMockRelationship({
-        sourceEntityId: entity1.id,
-        targetEntityId: entity2.id,
-        typeId: relationshipType1.id
+    const typeSalterTs = getType({
+        friendlyName: 'Salter TS',
+        color: '67C130',
+        icon: 'LineChart',
+        kind: Kind.TimeSeries,
+        propertyIds: [propFan.id, propHum.id]
     });
+    const typeDryerTs = getType({
+        friendlyName: 'Dryer',
+        color: 'FFA113',
+        icon: 'LineChart',
+        kind: Kind.TimeSeries,
+        propertyIds: [propInflow.id, propOutflow.id]
+    });
+    const typeFactory = getType({
+        friendlyName: 'Factory',
+        color: '732A9A',
+        icon: 'EMI',
+        kind: Kind.UserDefined,
+        propertyIds: []
+    });
+    const typePid = getType({
+        friendlyName: 'P&Id Type 1',
+        color: 'DE564E',
+        icon: 'SplitObject',
+        kind: Kind.PID,
+        propertyIds: [propXPos.id, propYPos.id]
+    });
+    const typePastAsset = getType({
+        friendlyName: 'Pasteurizer',
+        color: '3AA0EB',
+        icon: 'Manufacturing',
+        kind: Kind.Asset,
+        propertyIds: [propTemp.id, propPress.id]
+    });
+    data.types = [
+        typeDryerTs,
+        typeFactory,
+        typeSalterTs,
+        typePid,
+        typePastAsset,
+        typePastTs
+    ];
 
-    const data = {
-        entities: [entity1, entity2],
-        properties: [prop1],
-        relationshipTypes: [relationshipType1],
-        relationships: [relationship1],
-        types: [type1]
+    // ENTITIES
+    const entPastA1Ts = getEntity({
+        friendlyName: 'Pasteurizer A1',
+        sourceEntityId: 'Pasteurizer_A1',
+        typeId: typePastTs.id,
+        sourceConnectionString: 'cluser:c1;db:db1;table:t1',
+        values: { 'OEE Semantic Types': 'Uptime' }
+    });
+    const entPastA2Ts = getEntity({
+        friendlyName: 'Pasteurizer A2',
+        sourceEntityId: 'Pasteurizer_A2',
+        typeId: typePastTs.id,
+        sourceConnectionString: 'cluser:c1;db:db1;table:t1',
+        values: {}
+    });
+    const entSaltB1 = getEntity({
+        friendlyName: 'Salter B1',
+        sourceEntityId: 'Salter_B1',
+        typeId: typeSalterTs.id,
+        sourceConnectionString: 'cluser:c1;db:db1;table:t1',
+        values: {}
+    });
+    const entSaltB3 = getEntity({
+        friendlyName: 'Salter B3',
+        sourceEntityId: 'Salter_B3',
+        typeId: typeSalterTs.id,
+        sourceConnectionString: 'cluser:c1;db:db1;table:t1',
+        values: {}
+    });
+    const entDryC1 = getEntity({
+        friendlyName: 'Dryer C1',
+        sourceEntityId: 'Dryer_C1',
+        typeId: typeDryerTs.id,
+        sourceConnectionString: 'cluser:c1;db:db1;table:t1',
+        values: {}
+    });
+    const entRedmondFactory = getEntity({
+        friendlyName: 'Redmond Factory',
+        sourceEntityId: undefined,
+        typeId: typeFactory.id,
+        sourceConnectionString: undefined,
+        values: {}
+    });
+    const entPastA1Pid = getEntity({
+        friendlyName: 'past_a1',
+        sourceEntityId: 'past_a1',
+        typeId: typePid.id,
+        sourceConnectionString: 'https://myblob.co/P&ID.jpg',
+        values: { xPos: '12', yPos: '25' }
+    });
+    const entPastA2Pid = getEntity({
+        friendlyName: 'past_a2',
+        sourceEntityId: 'past_a2',
+        typeId: typePid.id,
+        sourceConnectionString: 'https://myblob.co/P&ID.jpg',
+        values: { xPos: '35', yPos: '12' }
+    });
+    const entPastA1Asset = getEntity({
+        friendlyName: 'Pasteurizer A1',
+        sourceEntityId: 'Pasteurizer_A1',
+        typeId: typePastAsset.id,
+        sourceConnectionString: undefined,
+        values: undefined
+    });
+    const entPastA2Asset = getEntity({
+        friendlyName: 'Pasteurizer A2',
+        sourceEntityId: 'Pasteurizer_A2',
+        typeId: typePastAsset.id,
+        sourceConnectionString: undefined,
+        values: undefined
+    });
+    data.entities = [
+        entDryC1,
+        entPastA1Asset,
+        entPastA1Pid,
+        entPastA1Ts,
+        entPastA2Asset,
+        entPastA2Pid,
+        entPastA2Ts,
+        entRedmondFactory,
+        entSaltB1,
+        entSaltB3
+    ];
+
+    // RELATIONSHIP TYPES
+    const relTypeReference = getMockRelationshipType({
+        name: 'References'
+    });
+    const relTypeContains = getMockRelationshipType({
+        name: 'References'
+    });
+    data.relationshipTypes = [relTypeReference, relTypeContains];
+
+    // RELATIONSHIPS
+    const relFactorySaltB3 = getMockRelationship({
+        sourceEntityId: entRedmondFactory.id,
+        targetEntityId: entSaltB3.id,
+        typeId: relTypeContains.id
+    });
+    const relFactoryPastA1Asset = getMockRelationship({
+        sourceEntityId: entRedmondFactory.id,
+        targetEntityId: entPastA1Asset.id,
+        typeId: relTypeContains.id
+    });
+    const relFactoryPastA2Ts = getMockRelationship({
+        sourceEntityId: entRedmondFactory.id,
+        targetEntityId: entPastA2Ts.id,
+        typeId: relTypeContains.id
+    });
+    const relPastA1AssetPastA1Ts = getMockRelationship({
+        sourceEntityId: entPastA1Asset.id,
+        targetEntityId: entPastA1Ts.id,
+        typeId: relTypeReference.id
+    });
+    const relPastA1AssetPastA1Pid = getMockRelationship({
+        sourceEntityId: entPastA1Asset.id,
+        targetEntityId: entPastA1Pid.id,
+        typeId: relTypeReference.id
+    });
+    const relPastA2AssetPastA2Ts = getMockRelationship({
+        sourceEntityId: entPastA2Asset.id,
+        targetEntityId: entPastA2Ts.id,
+        typeId: relTypeReference.id
+    });
+    const relPastA2AssetPastA2Pid = getMockRelationship({
+        sourceEntityId: entPastA2Asset.id,
+        targetEntityId: entPastA2Pid.id,
+        typeId: relTypeReference.id
+    });
+    data.relationships = [
+        relFactoryPastA1Asset,
+        relFactoryPastA2Ts,
+        relFactorySaltB3,
+        relPastA1AssetPastA1Pid,
+        relPastA1AssetPastA1Ts,
+        relPastA2AssetPastA2Pid,
+        relPastA2AssetPastA2Ts
+    ];
+};
+export const GET_DEFAULT_MOCK_WIZARD_DATA_CONTEXT = (
+    scenario?: 'Dairy'
+): IWizardDataContextState => {
+    // default to dairy
+    scenario = scenario ?? 'Dairy';
+
+    const data: IWizardDataContextState = {
+        entities: [],
+        properties: [],
+        relationshipTypes: [],
+        relationships: [],
+        types: []
     };
-    console.log('Creating mock data', data);
+    switch (scenario) {
+        case 'Dairy': {
+            setDairyData(data);
+        }
+    }
 
     return data;
 };
