de60bb20d1b66e8d7ed5be253a438025672c03da
Paul Montgomery <pamontg@microsoft.com>
2023-04-26 14:08:40 -0700

    Legion/montgomp/extension client (#939)
    * Remove eslint things that fail. Add extension client to LegionApp
    
    * Updated extensionClient
    
    * Reenable linter settings and fix PR comments
    
    * --amend

--- src/Apps/Legion/EntryPoints/LegionApp/LegionApp.types.ts
+++ src/Apps/Legion/EntryPoints/LegionApp/LegionApp.types.ts
@@ -1,2 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface ILegionAppProps {}
+import { IExtensionClient } from './ShellTypes';
+
+export interface ILegionAppProps {
+    extensionClient?: IExtensionClient;
+}
