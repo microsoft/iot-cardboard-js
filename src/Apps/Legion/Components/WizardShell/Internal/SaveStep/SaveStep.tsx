be8ad9945c3160ea7df29691484ba04b3030a87e
Ambika Dubey <ambika.dubey@microsoft.com>
2023-05-01 12:21:23 -0700

    initial

--- src/Apps/Legion/Components/WizardShell/Internal/SaveStep/SaveStep.tsx
+++ src/Apps/Legion/Components/WizardShell/Internal/SaveStep/SaveStep.tsx
@@ -5,9 +5,15 @@ import {
     ISaveStepStyles
 } from './SaveStep.types';
 import { getStyles } from './SaveStep.styles';
-import { classNamesFunction, styled } from '@fluentui/react';
+import { Stack, classNamesFunction, styled } from '@fluentui/react';
 import { getDebugLogger } from '../../../../../../Models/Services/Utils';
 import { useExtendedTheme } from '../../../../../../Models/Hooks/useExtendedTheme';
+import { useTranslation } from 'react-i18next';
+import {
+    IWizardAction,
+    WizardStepNumber
+} from '../../../../Contexts/WizardNavigationContext/WizardNavigationContext.types';
+import { useCustomNavigation } from '../../../../Hooks/useCustomNavigation';
 
 const debugLogging = false;
 const logDebugConsole = getDebugLogger('SaveStep', debugLogging);
@@ -25,6 +31,18 @@ const SaveStep: React.FC<ISaveStepProps> = (props) => {
     // state
 
     // hooks
+    const { t } = useTranslation();
+
+    /** Register wizard buttons */
+    const primaryAction: IWizardAction = {
+        disabled: false,
+        onClick: () => {
+            console.log('Save clicked');
+        },
+        iconName: 'Save',
+        text: t('save')
+    };
+    useCustomNavigation(WizardStepNumber.Save, primaryAction, null);
 
     // callbacks
 
@@ -37,7 +55,18 @@ const SaveStep: React.FC<ISaveStepProps> = (props) => {
 
     logDebugConsole('debug', 'Render');
 
-    return <div className={classNames.root}>Hello SaveStep!</div>;
+    return (
+        <div className={classNames.root}>
+            <Stack>
+                <p>{'6 Entities created'}</p>
+                <p>{'  - 5 Discovered'}</p>
+                <p>{'  - 1 Manually created'}</p>
+                <p>{'3 Types created'}</p>
+                <p>{'  - 2 time series entities'}</p>
+                <p>{'  - 1 user-defined entity'}</p>
+            </Stack>
+        </div>
+    );
 };
 
 export default styled<ISaveStepProps, ISaveStepStyleProps, ISaveStepStyles>(
