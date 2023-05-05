de5ebf5e49fb5a45fe558d8f0883b164fdc099b1
Ambika Dubey <ambika.dubey@microsoft.com>
2023-05-05 12:19:03 -0700

    recursive tree

--- src/Apps/Legion/Components/TreeView/TreeView.tsx
+++ src/Apps/Legion/Components/TreeView/TreeView.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { getDebugLogger } from '../../../../Models/Services/Utils';
 import { Asset, ITreeViewProps } from './TreeView.types';
-import { useClassNames } from './TreeView.styles';
+// import { useClassNames } from './TreeView.styles';
 import {
     Tree,
     TreeItem,
     TreeItemLayout
 } from '@fluentui/react-components/unstable';
+import { isDefined } from '../../Services/Utils';
 
 const debugLogging = false;
 const logDebugConsole = getDebugLogger('TreeView', debugLogging);
@@ -23,41 +24,28 @@ const TreeView: React.FC<ITreeViewProps> = (_props) => {
     // side effects
 
     // styles
-    const classNames = useClassNames();
+    // const classNames = useClassNames();
 
     logDebugConsole('debug', 'Render');
 
-    console.log('AAAAAAAA prop data', _props.sampleData);
+    const constructTree = (asset: Asset): JSX.Element => {
+        if (isDefined(asset)) {
+            return (
+                <TreeItem>
+                    <TreeItemLayout>{asset.name}</TreeItemLayout>
+                    {asset.childAssets.length > 0 && (
+                        <Tree>
+                            {asset.childAssets.map((c) => constructTree(c))}
+                        </Tree>
+                    )}
+                </TreeItem>
+            );
+        }
+    };
 
     return (
         <Tree aria-label="Tree">
-            <TreeItem>
-                <TreeItemLayout>level 1, item 1</TreeItemLayout>
-                <Tree>
-                    <TreeItem>
-                        <TreeItemLayout>level 2, item 1</TreeItemLayout>
-                    </TreeItem>
-                    <TreeItem>
-                        <TreeItemLayout>level 2, item 2</TreeItemLayout>
-                    </TreeItem>
-                    <TreeItem>
-                        <TreeItemLayout>level 2, item 3</TreeItemLayout>
-                    </TreeItem>
-                </Tree>
-            </TreeItem>
-            <TreeItem>
-                <TreeItemLayout>level 1, item 2</TreeItemLayout>
-                <Tree>
-                    <TreeItem>
-                        <TreeItemLayout>level 2, item 1</TreeItemLayout>
-                        <Tree>
-                            <TreeItem>
-                                <TreeItemLayout>level 3, item 1</TreeItemLayout>
-                            </TreeItem>
-                        </Tree>
-                    </TreeItem>
-                </Tree>
-            </TreeItem>
+            {_props.sampleData.map((x) => constructTree(x))}
         </Tree>
     );
 };
