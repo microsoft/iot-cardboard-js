3892b99fdc145dd9655380e49016440e6de8c6f8
iremgokce <iremay@microsoft.com>
2023-05-15 16:22:10 -0700

    reverted to fixed

--- src/Apps/Legion/Components/WizardShell/Internal/ModifyStep/Internal/DiagramTab/DiagramTab.styles.ts
+++ src/Apps/Legion/Components/WizardShell/Internal/ModifyStep/Internal/DiagramTab/DiagramTab.styles.ts
@@ -4,7 +4,7 @@ import { makeStyles, shorthands } from '@fluentui/react-components';
 export const useClassNames = makeStyles<TDiagramTabClassNames>({
     root: {},
     diagramSelector: {
-        width: '40%',
+        width: '308px',
         ...shorthands.padding('20px', '0')
     },
     diagramWrapper: {
