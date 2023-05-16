d357e0cf8ac25eb724ec196644f6144ec99d56a3
Ambika Dubey <ambika.dubey@microsoft.com>
2023-05-16 11:24:56 -0700

    remove comments

--- src/Apps/Legion/Components/TreeView/TreeView.tsx
+++ src/Apps/Legion/Components/TreeView/TreeView.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { getDebugLogger } from '../../../../Models/Services/Utils';
 import { Asset, ITreeViewProps } from './TreeView.types';
-// import { useClassNames } from './TreeView.styles';
 import {
     Tree,
     TreeItem,
@@ -23,44 +22,8 @@ const TreeView: React.FC<ITreeViewProps> = (_props) => {
     // side effects
 
     // styles
-    // const classNames = useClassNames();
 
     logDebugConsole('debug', 'Render');
-
-    // NOTE: Commented example is directly from the docs:
-    // https://react.fluentui.dev/?path=/docs/preview-components-tree-tree--default
-
-    // return (
-    //     <Tree aria-label="Tree">
-    //         <TreeItem>
-    //             <TreeItemLayout>level 1, item 1</TreeItemLayout>
-    //             <Tree>
-    //                 <TreeItem>
-    //                     <TreeItemLayout>level 2, item 1</TreeItemLayout>
-    //                 </TreeItem>
-    //                 <TreeItem>
-    //                     <TreeItemLayout>level 2, item 2</TreeItemLayout>
-    //                 </TreeItem>
-    //                 <TreeItem>
-    //                     <TreeItemLayout>level 2, item 3</TreeItemLayout>
-    //                 </TreeItem>
-    //             </Tree>
-    //         </TreeItem>
-    //         <TreeItem>
-    //             <TreeItemLayout>level 1, item 2</TreeItemLayout>
-    //             <Tree>
-    //                 <TreeItem>
-    //                     <TreeItemLayout>level 2, item 1</TreeItemLayout>
-    //                     <Tree>
-    //                         <TreeItem>
-    //                             <TreeItemLayout>level 3, item 1</TreeItemLayout>
-    //                         </TreeItem>
-    //                     </Tree>
-    //                 </TreeItem>
-    //             </Tree>
-    //         </TreeItem>
-    //     </Tree>
-    // );
 
     const generateTree = (data: Asset[]) => (
         <Tree aria-label="Tree">
