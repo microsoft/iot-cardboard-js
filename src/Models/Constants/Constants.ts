149f3f534d91ef16c89194e32fe930efcad8816a
Patricio Sanchez <pasanch@microsoft.com>
2025-07-17 15:53:07 -0700

    fix: Publish

--- src/Models/Constants/Constants.ts
+++ src/Models/Constants/Constants.ts
@@ -370,7 +370,6 @@ export const intellisenseMultilineBreakpoint = 40;
 
 export const CardboardClassNamePrefix = 'cb';
 
-// URLs for 3D models, production and development
 export const globeUrl =
     'https://iotcardboardjsresources.blob.core.windows.net/public/Globe.glb';
 export const devGlobeUrl =
