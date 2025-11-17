9dcec5bc5892f670fcfa766ba2995d898a240724
Mitch Snyder <msnyder@microsoft.com>
2025-11-17 11:16:41 -0800

    Clarify URL safety check comment
    Updated comment to clarify URL safety check.

--- src/Components/BehaviorsModal/Internal/Widgets/LinkWidget/LinkWidget.tsx
+++ src/Components/BehaviorsModal/Internal/Widgets/LinkWidget/LinkWidget.tsx
@@ -28,7 +28,7 @@ export const LinkWidget: React.FC<IProp> = ({ widget }) => {
               );
 
     const isSafeUrl = (url: string): boolean => {
-        // making sure the provided value is a real URL
+        // making sure the provided value is a real URL and safe to use
         try {
             const parsed = new URL(url);
             return ['http:', 'https:'].includes(parsed.protocol);
